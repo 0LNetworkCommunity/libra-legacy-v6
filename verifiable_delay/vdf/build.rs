35e839f57b6c62b7171c9b7d70da3dd5beec075b
Demi M. Obenour <demiobenour@gmail.com>
2018-11-20 15:23:08 -0500

    Document build.rs

--- verifiable_delay/vdf/build.rs
+++ verifiable_delay/vdf/build.rs
@@ -16,12 +16,22 @@ use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
 use std::u16;
+
+/// The number of odd prime less than 65536.
 const PRIMES_LEN: usize = 6541;
+
+/// The number of integers that are:
+///
+/// * equal to 7 mod 8
+/// * not divisible by any prime number less than or equal to 13.
+/// * less than 8 * 3 * 5 * 7 * 11 * 13
 const RESIDUES_LEN: usize = 5760;
+
+/// The number of odd prime numbers between 13 and 65536 exclusive.
 const SIEVE_INFO_LEN: usize = PRIMES_LEN - 5;
+
 fn odd_primes_below_n(n: usize) -> Vec<usize> {
-    let mut sieve = Vec::with_capacity(n >> 1);
-    sieve.resize(n >> 1, true);
+    let mut sieve = vec![true; n >> 1];
     let mut q = (n as f64).powf(0.5) as usize;
     assert!(q * q <= n);
     q += 1;
@@ -44,28 +54,38 @@ fn odd_primes_below_n(n: usize) -> Vec<usize> {
     res
 }
 
-// Probably horribly slow.  I don’t care.
-fn mod_exponentiation(q: usize, r: usize, s: usize) -> usize {
-    assert!(q < u16::MAX.into());
-    assert!(r < u16::MAX.into());
-    assert!(s < u16::MAX.into());
-    let (mut a, mut exponent, modulus) = (q as u32, r as u32, s as u32);
+fn mod_exponentiation(base: usize, exponent: usize, modulus: usize) -> usize {
+    assert!(base < u16::MAX.into());
+    assert!(exponent < u16::MAX.into());
+    assert!(modulus < u16::MAX.into());
+    let (mut base, mut exponent, modulus) = (base as u32, exponent as u32, modulus as u32);
     let mut state = 1;
     loop {
         if exponent & 1 != 0 {
-            state *= a;
+            state *= base;
             state %= modulus;
         }
         exponent >>= 1;
         if exponent == 0 {
             return state as _;
         }
-        a *= a;
-        a %= modulus;
+        base *= base;
+        base %= modulus;
     }
 }
 
+macro_rules! const_fmt {
+    () => {
+        "#[allow(clippy::unreadable_literals)]\n\
+         const {}: [{}; {}] = {:?};\n\n";
+    };
+}
 const M: usize = 8 * 3 * 5 * 7 * 11 * 13;
+fn emit<T: std::fmt::Debug>(f: &mut dyn Write, name: &str, t: &str, obj: &[T]) {
+    write!(f, const_fmt!(), name, t, obj.len(), obj).expect("i/o error in build script");
+}
+
+/// Write the generated code to `f`.
 fn generate(f: &mut dyn Write) {
     let odd_primes_below_65535 = odd_primes_below_n(1 << 16);
     let odd_primes_above_13 = &odd_primes_below_65535[5..];
@@ -81,27 +101,13 @@ fn generate(f: &mut dyn Write) {
         residues.push(x);
     }
     assert_eq!(residues.len(), RESIDUES_LEN);
-    write!(
-        f,
-        "#[cfg_attr(feature = \"cargo-clippy\", allow(clippy::unreadable_literals))]\n\
-         const RESIDUES: [u32; {}] = {:?};\n\n",
-        RESIDUES_LEN,
-        &residues[..]
-    )
-    .expect("i/o error");
+    emit(f, "RESIDUES", "u32", &residues[..]);
     let mut sieve_info = Vec::with_capacity(SIEVE_INFO_LEN);
     for &i in odd_primes_above_13 {
         sieve_info.push((i, mod_exponentiation(M % i, i - 2, i)));
     }
     assert_eq!(sieve_info.len(), SIEVE_INFO_LEN);
-    write!(
-        f,
-        "#[cfg_attr(feature = \"cargo-clippy\", allow(clippy::unreadable_literals))]\n\
-         const SIEVE_INFO: [(u16, u16); {}] = {:?};\n\n",
-        SIEVE_INFO_LEN,
-        &sieve_info[..]
-    )
-    .expect("i/o error");
+    emit(f, "SIEVE_INFO", "(u16, u16)", &sieve_info[..]);
 }
 
 fn main() {
