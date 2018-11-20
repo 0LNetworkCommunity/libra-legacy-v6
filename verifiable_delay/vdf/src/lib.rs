89d13a2ce6a745600b3b48ec981e4630f5f821f2
Demi M. Obenour <demiobenour@gmail.com>
2018-11-20 15:24:11 -0500

    Avoid `#[forbid(warnings)]`
    Sometimes, warnings are unavoidable, such as in automatically-generated
    code or when clippy has false positives.

--- verifiable_delay/vdf/src/lib.rs
+++ verifiable_delay/vdf/src/lib.rs
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // and limitations under the License.
-#![forbid(warnings)]
+#![deny(warnings)]
 extern crate gmp;
 extern crate libc;
 extern crate num_traits;
