2babd2daf9ef7b43c18dc45aa280fde8571e6a2d
Demi M. Obenour <demiobenour@gmail.com>
2018-11-23 17:57:21 -0500

    Add test harness
    This adds 468 different test cases, one for each length between 66 and
    1000 inclusive.  It also adds a shell script to test them with.  All
    tests pass.

--- verifiable_delay/check.sh
+++ verifiable_delay/check.sh
@@ -1,3 +1,39 @@
-#!/bin/sh --
-a=$(exec cargo run prove -- "$1" "$2") || exit $?
-exec ./target/debug/vdf-cli verify "$1" "$2" "$a"
+#!/bin/bash --
+set -euo pipefail
+
+# Change to the directory containing this script
+case $0 in
+   (/*) cd "${0%/*}/";;
+   (*/*) cd "./${0%/*}";;
+   (*) :;;
+esac
+
+cargo test
+cargo test --release
+IFS=, k=: count=0
+prove () {
+   exec ./target/release/vdf-cli prove -- "$@"
+}
+
+verify() {
+   exec ./target/release/vdf-cli verify -- "$@"
+}
+
+test_output () {
+   q=$1
+   shift
+   if correct_output=$("$@") && [[ "$correct_output" = "$q" ]]; then
+      echo SUCCESS
+   else
+      echo FAILED
+      k=false
+   fi
+}
+
+while read challenge iterations correct_proof; do
+   printf "Checking proof of input %d... " "$((count += 1))"
+   test_output "$correct_proof" prove "$challenge" "$iterations"
+   printf "Checking verification of input %d... " "$count"
+   test_output 'Proof is valid' verify "$challenge" "$iterations" "$correct_proof"
+done < <(grep -E '^[a-f0-9]+,[0-9]+,[0-9a-f]+$' test_data.csv)
+"$k"
