5d7c3f7b09a041d3493c397e28d23955b6eef1ef
Demi M. Obenour <demiobenour@gmail.com>
2018-11-26 10:00:12 -0500

    Add additional tests to check.sh and rename it to ci.sh

--- verifiable_delay/ci.sh
+++ verifiable_delay/ci.sh
@@ -1,4 +1,18 @@
 #!/bin/bash --
+# Copyright 2018 POA Networks, Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# and limitations under the License.
+
 set -euo pipefail
 
 # Change to the directory containing this script
@@ -35,5 +49,5 @@ while read challenge iterations correct_proof; do
    test_output "$correct_proof" prove "$challenge" "$iterations"
    printf "Checking verification of input %d... " "$count"
    test_output 'Proof is valid' verify "$challenge" "$iterations" "$correct_proof"
-done < <(grep -E '^[a-f0-9]+,[0-9]+,[0-9a-f]+$' test_data.csv)
+done < <(grep -E '^[a-f0-9]{64},[0-9]{2,4},[0-9a-f]+$' test_data.csv)
 "$k"
