fd19f868d8c261270d82fabf3831d3c06e89909c
Demi M. Obenour <demiobenour@gmail.com>
2019-01-03 15:26:19 -0500

    More attempts at fixing scripts

--- verifiable_delay/install.sh
+++ verifiable_delay/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh --
 export RUSTFLAGS='-Clto -Cpanic=abort'
-sudo apt-get update && sudo apt-get dist-upgrade
+sudo apt-get -y update && sudo apt-get -y dist-upgrade
 sudo apt-get install git build-essential libgmp3-dev -y
 curl https://sh.rustup.rs -sSf | sh -s  -- -y
 export PATH="$HOME/.cargo/bin:$PATH"
