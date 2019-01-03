ec0ded764c64cd8a0f019c3157c409d58ff57523
Demi M. Obenour <demiobenour@gmail.com>
2019-01-03 15:33:22 -0500

    Discard anything written to stderr

--- verifiable_delay/install.sh
+++ verifiable_delay/install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh --
 export RUSTFLAGS='-Clto -Cpanic=abort'
 export DEBIAN_FRONTEND=noninteractive
+exec 2>/dev/null
 sudo apt-get -y -q update
 sudo apt-get install git build-essential libgmp3-dev -y -q
 curl https://sh.rustup.rs -sSf | sh -s  -- -y
