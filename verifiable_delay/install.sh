3ffb988e87f21f94f65e0db660f696323214e754
Demi M. Obenour <demiobenour@gmail.com>
2019-01-03 15:23:40 -0500

    Force exit code 0

--- verifiable_delay/install.sh
+++ verifiable_delay/install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh --
 export RUSTFLAGS='-Clto -Cpanic=abort'
-sudo apt update
-sudo apt install git build-essential libgmp3-dev -y
+sudo apt-get update && sudo apt-get dist-upgrade
+sudo apt-get install git build-essential libgmp3-dev -y
 curl https://sh.rustup.rs -sSf | sh -s  -- -y
 export PATH="$HOME/.cargo/bin:$PATH"
 rustup install nightly
 rustup default nightly
-cargo install --force --path=vdf-competition
+cargo install --force --path=vdf-competition || :
 exit 0
