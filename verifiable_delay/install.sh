12606b42e988bf03be589e525380cb0b5187d932
Demi M. Obenour <demiobenour@gmail.com>
2019-01-03 14:33:33 -0500

    Fix install scripts

--- verifiable_delay/install.sh
+++ verifiable_delay/install.sh
@@ -1,8 +1,17 @@
 #!/bin/sh --
-set -eu
-unset CARGO_INCREMENTAL LD_LIBRARY_PATH LD_RUN_PATH RUST_BACKTRACE || :
+
+case $0 in
+   (/*) dir=${0/*}/;;
+   (*/*) dir=./${0/*};;
+   (*) dir=.;;
+esac
+cd "$dir"
+
+unset dir CARGO_INCREMENTAL LD_LIBRARY_PATH LD_RUN_PATH RUST_BACKTRACE
 export RUSTFLAGS='-Clto -Cpanic=abort'
-which apt-get >/dev/null 2>&1 && sudo apt-get -y install build-essential m4 curl gpg2 xz cargo
+
+which apt-get >/dev/null 2>&1 &&
+   sudo apt-get -y install build-essential m4 curl gnupg2 xz cargo gnupg gpg gpgv2
 which dnf > /dev/null 2>&1 && sudo dnf -y install gcc gpg xz cargo
 tmpdir=$(mktemp -d /var/tmp/vdf.XXXXXX)
 trap 'rm -rf -- "$tmpdir"' EXIT
@@ -24,12 +33,11 @@ gpgv2 --keyring=./pubring.kbx gmp-6.1.2.tar.xz.sig gmp-6.1.2.tar.xz || exit $?
 mkdir build 2>/dev/null || { rm -rf build && mkdir build; }
 tar xJvf gmp-6.1.2.tar.xz
 cd build
-../gmp-6.1.2/configure --prefix=/usr --libdir=/usr/lib64 CFLAGS='-O3 -march=native'
+libdir=/usr/lib
+test -d /usr/lib64 && libdir=/usr/lib64
+../gmp-6.1.2/configure --prefix=/usr "--libdir=$libdir" 'CFLAGS=-O3 -march=native'
 make -j2 && make check
 sudo make install
 sudo ldconfig
 
-mkdir rust
-tar -Crust -xJvf ~/QubesIncoming/Work-Code/output.tar.xz
-cd rust
 cargo install --force --path=vdf-competition
