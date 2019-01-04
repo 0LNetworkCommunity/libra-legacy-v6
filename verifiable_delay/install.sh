2bd401ebcc23ba2ff8a6544596d4a62aecbd6ce9
Igor Barinov <igor@poa.net>
2019-01-04 06:16:50 +0100

    removed upgrade from install.sh

--- verifiable_delay/install.sh
+++ verifiable_delay/install.sh
@@ -1,5 +1,4 @@
 #!/bin/sh --
-export RUSTFLAGS='-Clto -Cpanic=abort'
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get -y -q update
 sudo apt-get install git build-essential libgmp3-dev -y -q
@@ -7,5 +6,4 @@ curl https://sh.rustup.rs -sSf | sh -s  -- -y
 export PATH="$HOME/.cargo/bin:$PATH"
 rustup install nightly
 rustup default nightly
-cargo install --force --path=vdf-competition || :
-exit 0
+cargo install --force --path=vdf-competition
