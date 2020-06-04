343ea1a035d638dc6ddc936a3ac6a2fca8357b5e
lpgeiger <lucas.geiger@gmail.com>
2020-06-04 19:22:44 -0400

    moved block writing to separate module and test. Tests fail with 'Abscissa application state accessed before it has been init.'

--- verifiable_delay/ol-miner/src/block.rs
+++ verifiable_delay/ol-miner/src/block.rs
@@ -1,9 +1,9 @@
 //! Proof block datastructure
+
 use hex::{decode, encode};
 use serde::{de::Error, Deserialize, Deserializer, Serialize, Serializer};
-// use serde_json;
+use abscissa_core::{config, Command, FrameworkError, Options, Runnable};
 
-///Proof Block Datastructure
 #[derive(Serialize, Deserialize)]
 pub struct Block {
     ///Block Height
@@ -27,4 +27,60 @@ where
     let s: &str = Deserialize::deserialize(deserializer)?;
     // do better hex decoding than this
     decode(s).map_err(D::Error::custom)
+}
+
+pub mod build_block {
+    use glob::glob;
+    use std::{fs, io::Write, path::Path, path::PathBuf};
+    use super::Block;
+    use crate::prelude::*;
+
+    // writes a JSON file with the vdf proof, ordered by a blockheight
+    pub fn write_block(proof_data: Vec<u8>) {
+        let config = app_config();
+        let blocks_dir = Path::new(&config.chain_info.block_dir);
+
+        // parse the existing blocks in the app directory. It's a config in ol_miner.toml
+        fn parse_block_height (blocks_dir: &Path) -> (u64, Option<PathBuf>) {
+            if !blocks_dir.exists() {
+                fs::create_dir(blocks_dir).unwrap();
+                (0u64, None)
+            } else {
+                let mut max_block = 0u64;
+                let mut max_block_path = None;
+                for entry in glob(&format!("{}/block_*.json", blocks_dir.display()))
+                    .expect("Failed to read glob pattern")
+                {
+                    if let Ok(entry) = entry {
+                        if let Some(stem) = entry.file_stem() {
+                            if let Some(stem_string) = stem.to_str() {
+                                if let Some(blocknumber) = stem_string.strip_prefix("block_") {
+                                    let blocknumber = blocknumber.parse::<u64>().unwrap();
+                                    if blocknumber > max_block {
+                                        max_block = blocknumber;
+                                        max_block_path = Some(entry);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                (max_block, max_block_path)
+            }
+        };
+
+        // Write the file.
+        // serialize the data
+        let new_height = parse_block_height(blocks_dir).0 + 1;
+        let block = Block {
+            height: new_height,
+            data: proof_data,
+        };
+        // save a .json document
+        let mut latest_block_path = blocks_dir.to_path_buf();
+        latest_block_path.push(format!("block_{}.json", new_height));
+        let mut file = fs::File::create(&latest_block_path).unwrap();
+        file.write_all(serde_json::to_string(&block).unwrap().as_bytes())
+            .unwrap();
+    }
 }
