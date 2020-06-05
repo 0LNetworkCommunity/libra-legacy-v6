c41c187858c23ba8b82d3a44efe97f4a3fcf57e2
lpgeiger <lucas.geiger@gmail.com>
2020-06-04 21:42:49 -0400

    finish merge, move block building to module

--- verifiable_delay/ol-miner/src/block.rs
+++ verifiable_delay/ol-miner/src/block.rs
@@ -34,11 +34,12 @@ pub mod build_block {
     use std::{fs, io::Write, path::Path, path::PathBuf};
     use super::Block;
     use crate::prelude::*;
+    use libra_crypto::hash::HashValue;
+    use crate::delay::*;
+
 
     // writes a JSON file with the vdf proof, ordered by a blockheight
     pub fn write_block(proof_data: Vec<u8>) {
-        let config = app_config();
-        let blocks_dir = Path::new(&config.chain_info.block_dir);
 
         // parse the existing blocks in the app directory. It's a config in ol_miner.toml
         fn parse_block_height (blocks_dir: &Path) -> (u64, Option<PathBuf>) {
@@ -65,22 +66,55 @@ pub mod build_block {
                         }
                     }
                 }
-                (max_block, max_block_path)
+                return (max_block, max_block_path)
             }
         };
 
-        // Write the file.
-        // serialize the data
-        let new_height = parse_block_height(blocks_dir).0 + 1;
-        let block = Block {
-            height: new_height,
-            data: proof_data,
+        let config = app_config();
+        let blocks_dir = Path::new(&config.chain_info.block_dir);
+        let (mut current_block_number, current_block_path) = parse_block_height(blocks_dir);
+
+        let mut preimage ={
+            // If there is a previous block, use the last block as the preimage.
+            // This is the case where the app is picking up where it previously left off.
+            if let Some(max_block_path) = current_block_path{
+                let block_file =fs::read_to_string(max_block_path)
+                .expect("Could not read latest block");
+
+                let latest_block:Block = serde_json::from_str(&block_file)
+                .expect("could not deserialize latest block");
+
+                HashValue::sha3_256_of(&latest_block.data).to_vec()
+            // Otherwise this is the first time the app is run, and it needs a genesis preimage, which comes from configs.
+            }else{
+                HashValue::sha3_256_of(&config.gen_preimage()).to_vec()
+            }
         };
-        // save a .json document
-        let mut latest_block_path = blocks_dir.to_path_buf();
-        latest_block_path.push(format!("block_{}.json", new_height));
-        let mut file = fs::File::create(&latest_block_path).unwrap();
-        file.write_all(serde_json::to_string(&block).unwrap().as_bytes())
-            .unwrap();
+
+        loop{
+            status_ok!("Generating Proof for block {}",current_block_number.to_string());
+
+            let block = Block {
+                height: current_block_number + 1,
+                // note: do_delay() sigature is (challenge, delay difficulty)
+                data: delay::do_delay(&config.gen_preimage(),config.chain_info.block_size)
+            };
+            current_block_number +=1;
+
+            // set the preimage for the next loop.
+            preimage = HashValue::sha3_256_of(&block.data).to_vec();
+
+            // Write the file.
+            // serialize the data
+            let mut latest_block_path = blocks_dir.to_path_buf();
+            latest_block_path.push(format!("block_{}.json", current_block_number));
+            let mut file = fs::File::create(&latest_block_path).unwrap();
+
+            file.write_all(serde_json::to_string(&block).unwrap().as_bytes())
+                .expect("Could not write block");
+
+            let config = app_config();
+            let blocks_dir = Path::new(&config.chain_info.block_dir);
+        }
     }
 }
