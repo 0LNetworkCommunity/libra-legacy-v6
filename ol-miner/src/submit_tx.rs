13519596e53bbc867528d8385cc78f158e4fd2c5
lpgeiger <lpgeiger@users.noreply.github.com>
2020-06-26 16:41:30 -0400

    [broken] cleanup WIP.

--- ol-miner/src/submit_tx.rs
+++ ol-miner/src/submit_tx.rs
@@ -10,82 +10,46 @@ use libra_types::{
 };
 
 pub mod submit_tx {
-    pub fn create_account() {
+    pub fn submit_vdf_proof_tx_to_network() {
         //     //! Functions for submitting proofs on chain
 
         //     // 1.Set Private Key file
         //
-        //     let mnemonic_file_path = xyz
+        MINER_MNEMONIC = "./miner.mnemonic";
         //
         //     // 2. Format Transaction
         //     // 3. Sign Transaction
         //     // 4. Create Client Proxy
-                super::create_client_proxy()
-        //
-        //     // 5. Execute submission with ClientProxy
-        //     fn execute(
-        //         client: ClientProxy,
-        //         sender: &AccountData,
-        //         sender_ref_id: usize,
-        //         sequence_number: u64,
-        //         challenge: Vec<u8>,
-        //         difficulty: u64,
-        //         proof: Vec<u8> ) {
-        //     //submit transaction
-        //     match client.execute_send_proof(
-        //         sender,
-        //         sender_ref_id,
-        //         sequence_number,
-        //         challenge,
-        //         difficulty,
-        //         proof) {
-        //             Ok( _) => println!("succeed." ),
-        //             Err(e) => report_error("Failed to send proof", e),
-        //         }
-        //     }
-        //
-        //     // check tranaction submitted
+        // // TODO: This is copied from the Testsuite/client there may be a a different API.
+
+        let port = 2344; // TODO: get port from /0_config/ files
+        let sender;
+        let sender_ref_id;
+        let sequence_number;
+        let challenge;
+        let difficulty;
+        let proof;
+
+        // TODO: Replace this with LibraClient method, instead of clientproxy which appears to be for testing.
+
+        let libra_client = ClientProxy::new(
+            /* url */ &format!("http://localhost:{}", port),
+            /* faucet/admin keys */  &self.faucet_key.1,
+            /* sync_on_wallet_recovery */ false,
+            /* faucet server */ None,
+            /* menmonic file */ Some(MINER_MNEMONIC),
+            /* waypoint */ WaypointConfig::None, // TODO: Get from /0_Config/
+        );
+
+        libra_client.execute_send_proof(
+            // sender: &AccountData,
+            // sender_ref_id: usize,
+            // sequence_number: u64,
+            // challenge: Vec<u8>,
+            // difficulty: u64,
+            // proof: Vec<u8>
+        )
 
 
     }
 }
-
-fn create_client_proxy () {
-    // // TODO: This is copied from the Testsuite/client there may be a a different API.
-
-    // let test = ClientProxy::get_libra_wallet(Some("./client.mnemonic".to_string())).expect("nope");
-    let port = 2344; // TODO: get port from /0_config/ files
-    // TODO: Replace this with LibraClient method, instead of clientproxy which appears to be for testing.
-    //let waypoint = Waypoint::new_epoch_boundary(&li).unwrap();
-    let waypoint =
-        Waypoint::new_epoch_boundary(&LedgerInfo::mock_genesis(Some(ValidatorSet::empty())))
-            .unwrap();
-
-    let libra_client = ClientProxy::new(
-        /* url */ &format!("http://localhost:{}", port),
-        /* faucet/admin keys */  &self.faucet_key.1,
-        /* sync_on_wallet_recovery */ false,
-        /* faucet server */ None,
-        /* menmonic file */ Some("./client.mnemonic".to_string()),
-        /* waypoint */ WaypointConfig::None, // TODO: Get from /0_Config/
-    );
-}
-
-// fn test_waypoint() -> Waypoint {
-//     let li = LedgerInfo::new(
-//         BlockInfo::new(
-//             1,
-//             10,
-//             HashValue::random(),
-//             HashValue::random(),
-//             123,
-//             1000,
-//             Some(EpochState::empty()),
-//         ),
-//         HashValue::zero(),
-//     );
-//     Waypoint::new_epoch_boundary(&li).unwrap();
-// }
-// pub mod submit_tx {
-
-// }
