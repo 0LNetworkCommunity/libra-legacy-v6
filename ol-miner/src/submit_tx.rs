f5a15461b567de2c89e144aeda7d7281415c69a0
lpgeiger <lpgeiger@users.noreply.github.com>
2020-06-26 16:49:50 -0400

    notes

--- ol-miner/src/submit_tx.rs
+++ ol-miner/src/submit_tx.rs
@@ -11,16 +11,20 @@ use libra_types::{
 
 pub mod submit_tx {
     pub fn submit_vdf_proof_tx_to_network() {
-        //     //! Functions for submitting proofs on chain
+        //! Functions for submitting proofs on chain
 
-        //     // 1.Set Private Key file
-        //
+        // NOTE (LG): We're using a testing tool ClientProxy so that the miner has
+        // its own client connection to network.
+        // ClientProxy is an abstraction on top of Libra Client and other modules. (as with all testing tools) is unstable and in develoment.
+        // TODO? Replace this implementation with LibraClient method,
+        // instead of clientproxy which appears to be for testing.
+        // There are API's for test servers, including faucet etc, which should not be there production.
+
+        // 1.Set Private Key file
+
         MINER_MNEMONIC = "./miner.mnemonic";
-        //
-        //     // 2. Format Transaction
-        //     // 3. Sign Transaction
-        //     // 4. Create Client Proxy
-        // // TODO: This is copied from the Testsuite/client there may be a a different API.
+
+        // 2. Format Transaction
 
         let port = 2344; // TODO: get port from /0_config/ files
         let sender;
@@ -30,12 +34,11 @@ pub mod submit_tx {
         let difficulty;
         let proof;
 
-        // TODO: Replace this with LibraClient method, instead of clientproxy which appears to be for testing.
-
+        // create the ClientProxy, with credentials, and point to network with a waypoint.
         let libra_client = ClientProxy::new(
             /* url */ &format!("http://localhost:{}", port),
-            /* faucet/admin keys */  &self.faucet_key.1,
-            /* sync_on_wallet_recovery */ false,
+            /* association keys for testing faucet */  &self.faucet_key.1, // TODO This is not needed for OL.
+            /* sync_on_wallet_recovery */ false, // TODO (ZM): Should this be true?
             /* faucet server */ None,
             /* menmonic file */ Some(MINER_MNEMONIC),
             /* waypoint */ WaypointConfig::None, // TODO: Get from /0_Config/
