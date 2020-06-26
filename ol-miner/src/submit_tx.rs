ce447a86455bb593522c2ef59ab2ca56a804001e
lpgeiger <lpgeiger@users.noreply.github.com>
2020-06-26 19:55:12 -0400

    include node config. Try to get swarm info from file.

--- ol-miner/src/submit_tx.rs
+++ ol-miner/src/submit_tx.rs
@@ -1,20 +1,22 @@
 //! OlMiner submit_tx module
 #![forbid(unsafe_code)]
 
-use cli::client_proxy::ClientProxy;
-use libra_types::{
-    waypoint::Waypoint,
-    ledger_info::LedgerInfo,
-    ledger_info::BlockInfo,
-    on_chain_config::ValidatorSet
-};
-
-use crate::application::{MINER_MNEMONIC, SWARM_DEV_PORT};
-
-
 pub mod submit_tx {
+    use cli::client_proxy::ClientProxy;
+    use libra_config::config::NodeConfig;
+
+    // use crate::application::{MINER_MNEMONIC, DEFAULT_PORT};
+    const DEFAULT_PORT: u64 = 2344; // TODO: this will likely deprecated in favor of urls and discovery.
+    const MINER_MNEMONIC: &str = "./miner.mnemonic"; // TODO: change this
+    const DEFAULT_NODE: &str = "src/config/test_data/single.node.config.toml";
+    const ASSOCIATION_KEY_FILE: &str = "./mint.key"; // TODO: remove this
+
+
     pub fn submit_vdf_proof_tx_to_network(challenge: Vec<u8>, difficulty: u64, proof: Vec<u8>) {
         //! Functions for submitting proofs on chain
+
+
+        let mut expected = NodeConfig::load(DEFAULT_NODE).expect("Unable to load config");
 
         // NOTE (LG): We're using a testing tool ClientProxy so that the miner has
         // its own client connection to network.
@@ -24,23 +26,23 @@ pub mod submit_tx {
         // There are API's for test servers, including faucet etc, which should not be there production.
 
         // create the ClientProxy, with credentials, and point to network with a waypoint.
-        let libra_client = clientproxy::ClientProxy::new(
-            /* url */ &format!("http://localhost:{}", SWARM_DEV_PORT),
-            /* association keys for testing faucet */  &self.faucet_key.1, // TODO This is not needed for OL.
+        let mut libra_client = ClientProxy::new(
+            /* url */ &format!("http://localhost:{}", DEFAULT_PORT),
+            /* association account file */  ASSOCIATION_KEY_FILE, // TODO This is not needed for OL.
             /* sync_on_wallet_recovery */ false, // TODO (ZM): Should this be true?
             /* faucet server */ None,
-            /* menmonic file */ Some(MINER_MNEMONIC),
-            /* waypoint */ WaypointConfig::None, // TODO: Get from /0_Config/
+            /* menmonic file */ Some(MINER_MNEMONIC.to_string()),
+            None/* waypoint */  // TODO: Get from /0_Config/
         );
-
+        //
         // 2. Format Transaction
         // transaction data
         let sender;//: AccountData = ; // The miner's user's Address
         let sender_ref_id: usize = 0; // The user's index.???
         let sequence_number: u64 = 0; // should come from the network
 
-        self.accounts.get(sender_ref_id).unwrap();
-        let sequence_number = sender.sequence_number;
+        // self.accounts.get(sender_ref_id).unwrap();
+        // let sequence_number = sender.sequence_number;
 
         libra_client.execute_send_proof(
             sender, // sender: &AccountData,
