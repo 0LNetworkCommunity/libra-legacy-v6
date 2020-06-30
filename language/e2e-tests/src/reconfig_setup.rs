0d4e47e0aa3f5f7fa4c543b3d0c30f426dae6923
DRanade <github@devansh.sent.com>
2020-06-30 11:15:56 -0600

    trying to figure out validator config in e2e

--- language/e2e-tests/src/reconfig_setup.rs
+++ language/e2e-tests/src/reconfig_setup.rs
@@ -6,61 +6,29 @@ use libra_types::{
 };
 use stdlib::transaction_scripts::StdlibScript;
 use move_core_types::{
-    language_storage::TypeTag
+    language_storage::TypeTag, 
 };
 
 
-pub fn txn_fee_tx_mint(
+pub fn bulk_update(
     sender: &Account,
-    payee: &Account,
-    seq_num: u64,
+    alice: &Account,
+    bob: &Account,
+    carol: &Account,
+    sha: &Account,
+    ram: &Account,
+    seq_num: u64
 ) -> SignedTransaction {
     let mut args: Vec<TransactionArgument> = Vec::new();
     let type_vec: Vec<TypeTag> = Vec::new();
-    args.push(TransactionArgument::Address(payee.addr));
+    args.push(TransactionArgument::Address(alice.addr));
+    args.push(TransactionArgument::Address(bob.addr));
+    args.push(TransactionArgument::Address(carol.addr));
+    args.push(TransactionArgument::Address(sha.addr));
+    args.push(TransactionArgument::Address(ram.addr));
 
     sender.create_signed_txn_with_args(
-        StdlibScript::TxFeeTestMint
-            .compiled_bytes()
-            .into_vec(),
-        type_vec,
-        args,
-        seq_num,
-        19_000_000,
-        0,
-        LBR_NAME.to_owned(),
-    )
-}
-
-pub fn txn_fee_tx_move(
-    sender: &Account,
-    seq_num: u64,
-) -> SignedTransaction {
-    let args: Vec<TransactionArgument> = Vec::new();
-    let type_vec: Vec<TypeTag> = Vec::new();
-
-    sender.create_signed_txn_with_args(
-        StdlibScript::TxFeeTestMove
-            .compiled_bytes()
-            .into_vec(),
-        type_vec,
-        args,
-        seq_num,
-        19_000_000,
-        0,
-        LBR_NAME.to_owned(),
-    )
-}
-
-pub fn txn_fee_tx_distr(
-    sender: &Account,
-    seq_num: u64
-) -> SignedTransaction {
-    let args: Vec<TransactionArgument> = Vec::new();
-    let type_vec: Vec<TypeTag> = Vec::new();
-
-    sender.create_signed_txn_with_args(
-        StdlibScript::TxFeeTestDistr
+        StdlibScript::ReconfigBulkUpdate
             .compiled_bytes()
             .into_vec(),
         type_vec,
--- language/stdlib/transaction_scripts/reconfig_bulk_update.move
+++ language/stdlib/transaction_scripts/reconfig_bulk_update.move
@@ -4,13 +4,22 @@ script {
     use 0x0::Vector;
     use 0x0::ValidatorUniverse;
     fun main(account: &signer, alice: address, bob: address, carol: address,
-            sha: address, ram: address) {
-        // Initialize the nodes as validators
-        ValidatorUniverse::add_validator(alice);
-        ValidatorUniverse::add_validator(bob);
-        ValidatorUniverse::add_validator(carol);
-        ValidatorUniverse::add_validator(sha);
-        ValidatorUniverse::add_validator(ram);
+            sha: address, _ram: address) {
+        // // Initialize the nodes as validators
+        // ValidatorUniverse::add_validator(alice);
+        // ValidatorUniverse::add_validator(bob);
+        // ValidatorUniverse::add_validator(carol);
+        // ValidatorUniverse::add_validator(sha);
+        // ValidatorUniverse::add_validator(ram);
+
+        // let vec = Vector::empty();
+        // Vector::push_back<address>(&mut vec, alice);
+        // Vector::push_back<address>(&mut vec, bob);
+        // Vector::push_back<address>(&mut vec, carol);
+        // Vector::push_back<address>(&mut vec, sha);
+        // Vector::push_back<address>(&mut vec, ram);
+
+        // LibraSystem::bulk_update_validators(account, vec, 15, 19);
 
         // Tests on initial size of validators 
         Transaction::assert(LibraSystem::validator_set_size() == 5, 1000);
@@ -27,7 +36,7 @@ script {
         ValidatorUniverse::add_validator(carol);
         Transaction::assert(Vector::length<address>(&vec) == 3, 1);
 
-        LibraSystem::bulk_update_validators(account, vec, 15, 1);
+        LibraSystem::bulk_update_validators(account, vec, 15, 20);
 
         // Check if updates are done
         Transaction::assert(LibraSystem::validator_set_size() == 3, 1000);
