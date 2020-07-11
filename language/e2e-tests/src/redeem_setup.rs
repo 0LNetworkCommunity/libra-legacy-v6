209a5a8aafa720df239ef124f7674e1554557599
Devansh Ranade <46942312+DRanade@users.noreply.github.com>
2020-07-10 19:43:55 -0600

    Dr onboarding (#131)
    * Adding onboarding transaction script.
    
    * Native function checks for challenge and extracts auth_key and returns address, returns auth key
    
    * e2e redeem tests. rename modules, patch types.
    
    Co-authored-by: lpgeiger <lpgeiger@users.noreply.github.com>

--- language/e2e-tests/src/redeem_setup.rs
+++ language/e2e-tests/src/redeem_setup.rs
@@ -3,6 +3,7 @@ use libra_types::transaction::{SignedTransaction, TransactionArgument};
 use stdlib::transaction_scripts::StdlibScript;
 use crate::gas_costs;
 use libra_types::account_config::LBR_NAME;
+use move_core_types::account_address::AccountAddress;
 
 /// This is test infrastructure. Helps build a signed transaction script of the Redeem module.
 pub fn redeem_txn(sender: &Account, seq_num: u64, challenge: Vec<u8>, difficulty: u64, solution: Vec<u8>, tower_height: u64 ) -> SignedTransaction {
@@ -26,19 +27,21 @@ pub fn redeem_txn(sender: &Account, seq_num: u64, challenge: Vec<u8>, difficulty
     )
 }
 
-/// This is test infrastructure. Helps build a signed transaction script TO INITIALIZE THE REDEEM module
-/// TODO: This may not be necessary if the Redeem module is initialized in the Genesis.move
-pub fn redeem_initialize_txn(sender: &Account, seq_num: u64 ) -> SignedTransaction {
-    let args = vec![];
-
+pub fn redeem_txn_onboarding(sender: &Account, seq_num: u64, challenge: Vec<u8>, difficulty: u64, solution: Vec<u8>, expected_address: AccountAddress) -> SignedTransaction {
+    let args = vec![
+        TransactionArgument::U8Vector(challenge),
+        TransactionArgument::U64(difficulty),
+        TransactionArgument::U8Vector(solution),
+        TransactionArgument::Address(expected_address),
+    ];
     sender.create_signed_txn_with_args(
-        StdlibScript::RedeemInitialize
+        StdlibScript::RedeemOnboarding
             .compiled_bytes()
             .into_vec(),
         vec![],
         args,
         seq_num,
-        gas_costs::TXN_RESERVED,
+        gas_costs::TXN_RESERVED * 4,
         0,
         LBR_NAME.to_owned(),
     )
--- language/move-lang/functional-tests/tests/OL/redeem/begin_redeem_create_account.move
+++ language/move-lang/functional-tests/tests/OL/redeem/begin_redeem_create_account.move
@@ -10,20 +10,18 @@ use 0x0::LibraAccount;
 use 0x0::GAS;
 use 0x0::Transaction;
 
+
 fun main(sender: &signer) {
   let challenge = x"232fb6ae7221c853232fb6ae7221c853000000000000000000000000DEADBEEF";
-  let new_account_address = 0xDEADBEEF;
-  let auth_key_prefix = x"232fb6ae7221c853232fb6ae7221c853";
-
   // Parse key and check
-  Redeem::first_challenge_includes_address(new_account_address, challenge);
+  let (parsed_address, auth_key) = Redeem::address_from_challenge(&challenge);
   // GOAL: it would be ideal that these accounts could be created by any Alice, for any Bob, i.e.
   // if it didn't need to be the association or system account.
   //  ^ I think this is working with `create_validator_account_from_mining_0L`
-  LibraAccount::create_validator_account_from_mining_0L<GAS::T>(sender, new_account_address, auth_key_prefix);
+  LibraAccount::create_validator_account_from_mining_0L<GAS::T>(sender, parsed_address, auth_key);
 
   // Check the account exists and the balance is 0
-  Transaction::assert(LibraAccount::balance<GAS::T>(0xDEADBEEF) == 0, 0);
+  Transaction::assert(LibraAccount::balance<GAS::T>(parsed_address) == 0, 0);
 
 }
 }
