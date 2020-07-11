a5f909bcab27904ff0bb84ce3a3bdf326b2751dc
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2020-07-10 21:58:02 -0400

    Revert "Dr onboarding (#131)" (#133)
    This reverts commit 209a5a8aafa720df239ef124f7674e1554557599.

--- language/e2e-tests/src/lib.rs
+++ language/e2e-tests/src/lib.rs
@@ -20,13 +20,12 @@ pub mod data_store;
 pub mod executor;
 pub mod gas_costs;
 pub mod keygen;
-mod proptest_types;
-//0L CHanges
-pub mod redeem_setup;
+pub mod redeem;
 pub mod librablock_setup;
 pub mod txfee_setup;
 pub mod txn_fee_setup;
 pub mod reconfig_setup;
+mod proptest_types;
 
 pub fn assert_status_eq(s1: &VMStatus, s2: &VMStatus) -> bool {
     assert_eq!(s1.major_status, s2.major_status);
--- language/stdlib/src/transaction_scripts.rs
+++ language/stdlib/src/transaction_scripts.rs
@@ -60,8 +60,7 @@ pub enum StdlibScript {
     UpdateMintingAbility,
     // ...add new 0L scripts here
     Redeem,
-    RedeemOnboarding,
-    // RedeemInitialize,
+    RedeemInitialize,
     LibraBlockTestHelper, // 0L Change
     TxFeeTestHelper,// 0L Change
     TxFeeTestMint,  // 0L Change
@@ -114,8 +113,7 @@ impl StdlibScript {
             UpdateMintingAbility,
             // ...add new 0L scripts here
             Redeem,
-            RedeemOnboarding,
-            // RedeemInitialize,
+            RedeemInitialize,
             LibraBlockTestHelper, // 0L Change
             TxFeeTestHelper,    // 0L Change
             TxFeeTestMint,      // 0L Change
@@ -245,15 +243,14 @@ impl fmt::Display for StdlibScript {
                 UpdateExchangeRate => "update_exchange_rate",
                 UpdateMintingAbility => "update_minting_ability",
                 // 0L Changes
-                Redeem => "ol_redeem", // 0L Change
-                RedeemOnboarding => "ol_redeem_onboarding", // 0L Change
-                // RedeemInitialize => "redeem_init", // 0L Change
-                LibraBlockTestHelper => "ol_libra_block_test_helper", // 0L Change
-                TxFeeTestHelper => "ol_tx_fees_e2e_test_helper", // 0L Change
-                TxFeeTestMint => "ol_txn_fee_test_mint",       // 0L Change
-                TxFeeTestMove => "ol_txn_fee_test_move",       // 0L Change
-                TxFeeTestDistr => "ol_txn_fee_test_distr",     // 0L Change
-                ReconfigBulkUpdate => "ol_reconfig_bulk_update_e2e_test_helper"// 0L Change
+                Redeem => "redeem", // 0L Change
+                RedeemInitialize => "redeem_init", // 0L Change
+                LibraBlockTestHelper => "libra_block_test_helper", // 0L Change
+                TxFeeTestHelper => "tx_fees_e2e_test_helper",// 0L Change
+                TxFeeTestMint => "txn_fee_test_mint",       // 0L Change
+                TxFeeTestMove => "txn_fee_test_move",       // 0L Change
+                TxFeeTestDistr => "txn_fee_test_distr",     // 0L Change
+                ReconfigBulkUpdate => "reconfig_bulk_update"// 0L Change
             }
         )
     }
