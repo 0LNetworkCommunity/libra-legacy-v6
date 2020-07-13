48a1c86dd411784a0888f816cbce8b861a8064c3
liangping <18786721@qq.com>
2020-07-13 10:01:34 +0800

    Resolve the conflicts

--- testsuite/cli/src/client_proxy.rs
+++ testsuite/cli/src/client_proxy.rs
@@ -13,7 +13,7 @@ use libra_crypto::{
     traits::ValidCryptoMaterial,
     x25519, ValidCryptoMaterialStringExt,
 };
-use libra_json_rpc_client::views::{AccountView, BlockMetadata, EventView, TransactionView};
+use libra_json_rpc_client::views::{AccountView, BlockMetadata, EventView, TransactionView, MinerStateView};
 use libra_logger::prelude::*;
 use libra_network_address::{NetworkAddress, RawNetworkAddress};
 use libra_temppath::TempPath;
@@ -36,7 +36,7 @@ use libra_types::{
     vm_error::StatusCode,
     waypoint::Waypoint,
 };
-use libra_wallet::{io_utils, WalletLibrary, ChildNumber};
+use libra_wallet::{io_utils, WalletLibrary};
 use num_traits::{
     cast::{FromPrimitive, ToPrimitive},
     identities::Zero,
@@ -275,7 +275,7 @@ impl ClientProxy {
 
     /// 0L: Send a VDF proof from the Libra Shell with delimited strings
     /// Wraps execute_send_proof
-    pub fn send_proof(&mut self, space_delim_strings: &[&str], is_blocking: bool) -> Result<()> {
+    pub fn send_proof(&mut self, space_delim_strings: &[&str], _is_blocking: bool) -> Result<()> {
         ensure!(
             space_delim_strings.len() != 6 ,
             "Invalid number of arguments for sending proof"
@@ -301,6 +301,23 @@ impl ClientProxy {
             false
         )?;
         Ok(())
+    }
+
+    /// 0L: Send a VDF proof from the Libra Shell with delimited strings
+    /// Wraps execute_send_proof
+    pub fn get_miner_state(&mut self, space_delim_strings: &[&str]) -> Option<MinerStateView> {
+        // ensure!(
+        //     space_delim_strings.len() != 6 ,
+        //     "Invalid number of arguments for sending proof"
+        // );
+
+        println!("Debug: get miner state \n\nargs: {:?}", space_delim_strings );
+
+        let (sender_address, _) =
+            self.get_account_address_from_parameter(space_delim_strings[1]).unwrap();
+
+        let response = self.client.get_miner_state(sender_address );
+        response.unwrap()
     }
 
 
