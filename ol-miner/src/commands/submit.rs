1b48de212aecad49d69314d9f24557cb3139e405
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2020-07-16 17:20:59 -0400

    Client query miner state (#146)
    * Spec for error handling, retry, and resubmit backlog.
    
    * add `query miner state` on json-rpc service side
    
    * integrate `query miner state` into libra client.
    
    * Update retrieving of waypoint by ol-miner
    
    Co-authored-by: Zaki Manian <zaki@manian.org>
    Co-authored-by: liangping <18786721@qq.com>

--- ol-miner/src/commands/submit.rs
+++ ol-miner/src/commands/submit.rs
@@ -41,18 +41,27 @@ impl Runnable for SubmitCmd {
             Ok(line) => {
                 println!("Mnemonic: \n{}", line);
 
+                let waypoint: Waypoint;
                 let parsed_waypoint: Result<Waypoint, Error> = self.waypoint.parse();
                 match parsed_waypoint {
                     Ok(v) => {
                         println!("Using Waypoint from CLI args:\n{}", v);
+                        waypoint = parsed_waypoint.unwrap();
                     }
                     Err(e) => {
-                        println!("Waypoint cannot be parsed, check delimiter: Error:\n{:?}\n WILL FALLBACK TO WAYPOINT FROM ol_miner.toml", miner_configs.chain_info.base_waypoint);
-                        return;
+                        println!("Error: Waypoint cannot be parsed from command line args. Received: {:?}\nDid you pass --waypoint=0:<hash>? \n WILL FALLBACK TO WAYPOINT FROM ol_miner.toml\n {:?}",
+                        self.waypoint,
+                        miner_configs.chain_info.base_waypoint);
+                        waypoint = miner_configs.chain_info.base_waypoint.parse().unwrap();
+
                     }
                 }
 
-                build_block::submit_block(&miner_configs, line, parsed_waypoint.unwrap(),self.height);
+                build_block::submit_block(
+                    &miner_configs,
+                    line,
+                    waypoint,
+                    self.height);
             }
             Err(ReadlineError::Interrupted) => {
                 println!("CTRL-C");
