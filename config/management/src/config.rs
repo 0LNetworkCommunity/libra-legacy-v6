808edbd0e63c0c8a130d1c9d21769a63a680fed5
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2020-07-22 23:55:52 -0400

    Lg node configs (#167)
    * export config to toml file.
    
    * add fixtures
    
    * test adding waypoint to key_store.json
    
    * updated readme
    
    * Adding base director and genesis blob
    
    * Adding waypoint from storage backend
    
    * Fixing waypoint to be stored in local as well as remote.
    
    Co-authored-by: Andrew Dickson <adickson@gmail.com>
    Co-authored-by: Keerthi Nelaturu <nelaturuk@gmail.com>

--- config/management/src/config.rs
+++ config/management/src/config.rs
@@ -8,7 +8,8 @@ use libra_config::{
     },
     network_id::NetworkId,
 };
-use std::convert::TryInto;
+// use std::convert::TryInto;
+use std::{convert::TryInto, fs, fs::File, io::Write, net::SocketAddr, path::PathBuf};
 
 
 #[derive(Debug, StructOpt)]
@@ -31,11 +32,32 @@ impl Config {
 
         let mut config = NodeConfig::default();
 
+        // NOTE: There's something strange with calling libra-node from a path different from where this storage is located.
+
+
+        //TODO:
+        // Check consensus safety_rules
+        // check storage Paths
+        // where to output config.toml file
+        // how to add seed peers file.
+        //path to genesis.blob
+        // waypoint.
+        // [base.waypoint]
+        // type = "from_config"
+        //
+        // [base.waypoint.waypoint]
+        // version = 0
+        // value = "c20d50e14ca7cd0ef8fc209033f3f9ef7c0d0a169267cea8ec4ccda942868e19"
+
         let mut network = NetworkConfig::network_with_id(NetworkId::Validator);
+        println!("network\n{:?}", network);
+
         network.discovery_method = DiscoveryMethod::Onchain;
         config.validator_network = Some(network);
 
         let mut network = NetworkConfig::network_with_id(NetworkId::vfn_network());
+        println!("network\n{:?}", network);
+
         network.discovery_method = DiscoveryMethod::Onchain;
         config.full_node_networks = vec![network];
 
@@ -62,19 +84,33 @@ impl Config {
 
         config.consensus.safety_rules.backend = self.backend.backend.clone().try_into().unwrap();
 
-
+        // Load waypoint
+        config.base.waypoint = WaypointConfig::FromStorage { backend: self.backend.backend.clone().try_into().unwrap() };
+        
+        // Adding genesis file location
+        config.execution.genesis_file_location = PathBuf::from("genesis.blob");
 
         //TODO: The data is unecessary here, but may be good to include the actual data.
         config.configs_ol_miner.preimage ="".to_string();
         config.configs_ol_miner.proof ="".to_string();
 
+        // TODO: place in path with other files.
+        // Save file
+        let output_dir = PathBuf::from("./");
+
+        fs::create_dir_all(&output_dir).expect("Unable to create output directory");
+        config
+            .save(&output_dir.join("node.configs.toml"))
+            .expect("Unable to save node configs");
 
         Ok(toml::to_string_pretty(&config).unwrap())
-
-
-
-
-
     }
 
 }
+
+// fn save_node_config(mut node_config: NodeConfig, output_dir: &PathBuf) {
+//     fs::create_dir_all(output_dir).expect("Unable to create output directory");
+//     node_config
+//         .save(output_dir.join(NODE_CONFIG))
+//         .expect("Unable to save node configs");
+// }
