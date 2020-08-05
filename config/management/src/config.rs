4e00c3543ed01d5a7f59f1f1defd8317c384495d
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2020-08-05 19:47:24 -0400

    testing: return fullnode settings

--- config/management/src/config.rs
+++ config/management/src/config.rs
@@ -119,14 +119,14 @@ impl Config {
         }
 
 
-        // let fullnode_network = &mut config.full_node_networks[0];
-        // fullnode_network.listen_address = self.fullnode_listen_address;
-        // fullnode_network.advertised_address = self.fullnode_address;
-        // fullnode_network.identity = Identity::from_storage(
-        //     libra_global_constants::FULLNODE_NETWORK_KEY.into(),
-        //     libra_global_constants::OPERATOR_ACCOUNT.into(),
-        //     self.backend.backend.clone().try_into().unwrap(),
-        // );
+        let fullnode_network = &mut config.full_node_networks[0];
+        fullnode_network.listen_address = self.fullnode_listen_address;
+        fullnode_network.advertised_address = self.fullnode_address;
+        fullnode_network.identity = Identity::from_storage(
+            libra_global_constants::FULLNODE_NETWORK_KEY.into(),
+            libra_global_constants::OPERATOR_ACCOUNT.into(),
+            self.backend.backend.clone().try_into().unwrap(),
+        );
 
 
         config.consensus.safety_rules.backend = self.backend.backend.clone().try_into().unwrap();
