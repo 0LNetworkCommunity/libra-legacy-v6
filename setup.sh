3a64ca6291427a2f608967d89bca90da52000e6b
root <1364012+lpgeiger@users.noreply.github.com>
2020-08-06 21:24:46 +0000

    bash file for basic env config

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs --output rust_setup.sh
 bash rust_setup.sh --default-toolchain nightly -y
 source $HOME/.cargo/env
 
-echo "Enter ssh key for github:"
+echo "Enter ssh key for github (ctrl+d when done)"
 private_key=$(cat)
 echo $private_key > ~/.ssh/id_ed25519
 chmod 600 ~/.ssh/id_ed25519
