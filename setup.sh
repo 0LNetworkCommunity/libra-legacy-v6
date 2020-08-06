52c71dd2445c945c6baa6ba38e287f3f2d1232ae
root <1364012+lpgeiger@users.noreply.github.com>
2020-08-06 22:42:13 +0000

    setup sh and makefile

--- setup.sh
+++ setup.sh
@@ -3,15 +3,16 @@
 # targeting ububtu
 
 apt update
-apt install -y git cmake
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs --output rust_setup.sh
-bash rust_setup.sh --default-toolchain nightly -y
+apt install -y git nano build-essential cmake clang llvm libgmp-dev
+curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain nightly -y
 source $HOME/.cargo/env
 
 echo "Enter ssh key for github (ctrl+d when done)"
 private_key=$(cat)
 echo $private_key > ~/.ssh/id_ed25519
 chmod 600 ~/.ssh/id_ed25519
-ssh-keygen -t ed25519 -y -f ~/.ssh/id_ed25519 > ~/.ssh/id_ed25519.pub
+
+# TODO: make the public key
+# ssh-keygen -t ed25519 -y -f ~/.ssh/id_ed25519 > ~/.ssh/id_ed25519.pub
 
 git clone git@github.com:OLSF/libra.git
