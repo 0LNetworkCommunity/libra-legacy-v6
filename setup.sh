6a13971d6ad2fe3b3c0cbf9ed7b6f03150abd566
root <1364012+lpgeiger@users.noreply.github.com>
2020-08-06 21:20:36 +0000

    ubuntu install script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# targeting ububtu
+
+apt update
+apt install -y git cmake
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs --output rust_setup.sh
+bash rust_setup.sh --default-toolchain nightly -y
+source $HOME/.cargo/env
+
+echo "Enter ssh key for github:"
+private_key=$(cat)
+echo $private_key > ~/.ssh/id_ed25519
+chmod 600 ~/.ssh/id_ed25519
+ssh-keygen -y -f ~/.ssh/id_ed25519 > ~/.ssh/id_ed25519.pub
+
+git clone git@github.com:OLSF/libra.git
