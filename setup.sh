be076865b4cbe657ea040bf08ffe8715a73307bd
root <1364012+lpgeiger@users.noreply.github.com>
2020-08-07 01:21:12 +0000

    remove sample variables from make file.

--- setup.sh
+++ setup.sh
@@ -7,12 +7,10 @@ apt install -y git nano build-essential cmake clang llvm libgmp-dev
 curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain nightly -y
 source $HOME/.cargo/env
 
-echo "Enter ssh key for github (ctrl+d when done)"
+echo "Enter ssh id_rsa key for github (ctrl+d when done)"
 private_key=$(cat)
-echo $private_key > ~/.ssh/id_ed25519
-chmod 600 ~/.ssh/id_ed25519
-
-# TODO: make the public key
-# ssh-keygen -t ed25519 -y -f ~/.ssh/id_ed25519 > ~/.ssh/id_ed25519.pub
+echo $private_key > ~/.ssh/id_rsa
+chmod 400 ~/.ssh/id_rsa
+ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub
 
 git clone git@github.com:OLSF/libra.git
