b48136485f6c8abffe53c074fa7667a4204a2db5
liangping <18786721@qq.com>
2020-08-09 22:15:36 +0800

    load configs from node.config.toml

--- miner/src/commands/submit.rs
+++ miner/src/commands/submit.rs
@@ -1,27 +1,33 @@
 // `submit` subcommand
 
-
-
 use abscissa_core::{Command, Options, Runnable};
 use crate::prelude::*;
 use libra_types::{waypoint::Waypoint, account_address::AccountAddress};
 use libra_crypto::{
-    ed25519::{Ed25519PrivateKey, Ed25519PublicKey},
-    // test_utils::KeyPair,
+    ed25519::{Ed25519PrivateKey, Ed25519PublicKey, Ed25519Signature},
+    test_utils::KeyPair,
+    PrivateKey,
 };
 use libra_crypto::test_utils;
 use anyhow::Error;
-
-
-
+use language_e2e_tests::{
+    account::{Account, AccountData, AccountTypeSpecifier},
+    keygen::KeyGen,
+};
 use cli::{libra_client::LibraClient, AccountData};
 use reqwest::Url;
-use test_utils::KeyPair;
+use std::path::PathBuf;
+use libra_config::config::NodeConfig;
+use libra_types::transaction::{Script, TransactionArgument, TransactionPayload};
+use libra_types::transaction::helpers::*;
 
 #[derive(Command, Debug, Default, Options)]
 pub struct SubmitCmd {
     #[options(help = "Provide a waypoint for the libra chain")]
     waypoint: String, //Option<Waypoint>,
+
+    #[options(help = "Path of swarm config directory.")]
+    path: PathBuf,
 
     #[options(help = "Already mined height to submit")]
     height: usize,
@@ -30,12 +36,12 @@ pub struct SubmitCmd {
 impl Runnable for SubmitCmd {
     fn run(&self) {
         println!("TESTING SUBMITTING WITH KEYPAIR TO SWARM");
-        submit_test();
+        submit_test(self.path.clone());
     }
 
 }
 
-fn submit_test () -> Result<String,Error>{
+fn submit_test(mut config_path: PathBuf ) -> Result<String, Error> {
     let miner_configs = app_config();
 
     let _height = 1u64;
@@ -44,81 +50,92 @@ fn submit_test () -> Result<String,Error>{
     // let reader = BufReader::new(file);
     // let block: Block = serde_json::from_reader(reader).unwrap();
 
-    let hex_literal = format!("0x{}", &miner_configs.profile.account);
-    let account_address = AccountAddress::from_hex_literal(&hex_literal).unwrap();
-    dbg!(&account_address);
+    // let hex_literal = format!("0x{}", &miner_configs.profile.account);
+    // let account_address = AccountAddress::from_hex_literal(&hex_literal).unwrap();
+    // dbg!(&account_address);
+    //
+    // let url = miner_configs.chain_info.node.as_ref().unwrap().parse::<Url>();
+    // // let url: Result<Url, Error> = miner_configs.chain_info.node;
+    // let parsed_waypoint: Result<Waypoint, Error> = miner_configs.chain_info.base_waypoint.parse();
+    //
+    // //unwrap().parse::<Waypoint>();
+    // let auth_key = &miner_configs.profile.auth_key;
+    // dbg!(auth_key);
+    // let privkey = &miner_configs.profile.operator_private_key;
+    // // let operator_keypair = Some(AccountKeyPair::load(privkey));
+    // dbg!(privkey);
 
-    let url = miner_configs.chain_info.node.as_ref().unwrap().parse::<Url>();
-    // let url: Result<Url, Error> = miner_configs.chain_info.node;
-    let parsed_waypoint: Result<Waypoint, Error> = miner_configs.chain_info.base_waypoint.parse();
-    
-    //unwrap().parse::<Waypoint>();
-    let auth_key = &miner_configs.profile.auth_key;
-    dbg!(auth_key);
-    let privkey = &miner_configs.profile.operator_private_key;
-    // let operator_keypair = Some(AccountKeyPair::load(privkey));
-    dbg!(privkey);
+    config_path.push("0/node.config.toml");
+
+    let config = NodeConfig::load(&config_path)
+        .unwrap_or_else(|_| panic!("Failed to load NodeConfig from file: {:?}", config_path));
+    match &config.test {
+        Some( conf) => {
+            println!("Swarm Keys : {:?}", conf);
+        },
+        None =>{
+            println!("test config does not set.");
+        }
+    }
 
     // Create a client object
 
-    let mut client = LibraClient::new(url.unwrap(), parsed_waypoint.unwrap()).unwrap();
-    let account_state = client.get_account_state(account_address, true).unwrap();
+    let mut client = LibraClient::new(
+        Url::parse(format!("http://localhost:{}", config.rpc.address.port()).as_str()).unwrap(),
+        config.base.waypoint.waypoint_from_config().unwrap().clone()
+    ).unwrap();
+    let auth_key = config.test.unwrap().auth_key.unwrap();
+    let address = auth_key.derived_address();
+    let account_state = client.get_account_state(address.clone(), true).unwrap();
     dbg!(&account_state);
 
-    let mut sequence_number= 0u64;
+    let private_key = config.test.unwrap().operator_keypair.unwrap();
+
+    let mut sequence_number = 0u64;
     if account_state.0.is_some() {
         sequence_number = account_state.0.unwrap().sequence_number;
     }
     dbg!(&sequence_number);
 
-
-    // // get a key key pair to use for signing transactions.
-    //    let key_pair = KeyPair {
-    //     /// the private key component
-    //     private_key: privkey,
-    //     /// the public key component
-    //     public_key: Ed25519PublicKey.from(privkey),
-    // };
-    // dbg!(key_pair);
-
-
-    // // get account_data struct
+    // get account_data struct
     // let sender_account_data = AccountData {
-    //     account_address,
-    //     authentication_key: auth_key,
-    //     key_pair,
+    //     address,
+    //     authentication_key: Some(auth_key.to_vec()),
+    //     key_pair: Some(KeyPair{private_key: operator_key.take_private(), public_key: operator_key.public_key()))},
     //     sequence_number,
     //     status,
     // };
 
-    // // Create the unsigned MinerState transaction script
-    // let script = Script::new(
-    //     StdlibScript::Redeem.compiled_bytes().into_vec(),
-    //     vec![],
-    //     vec![
-    //         TransactionArgument::U8Vector(challenge),
-    //         TransactionArgument::U64(difficulty),
-    //         TransactionArgument::U8Vector(proof),
-    //         TransactionArgument::U64(tower_height),
-            
-    //     ],
-    // );
+    // Create the unsigned MinerState transaction script
+    let script = Script::new(
+        StdlibScript::Redeem.compiled_bytes().into_vec(),
+        vec![],
+        vec![
+            TransactionArgument::U8Vector(challenge),
+            TransactionArgument::U64(difficulty),
+            TransactionArgument::U8Vector(proof),
+            TransactionArgument::U64(tower_height),
+        ],
+    );
 
-    // // sign the transaction script
-    // let txn = create_user_txn(
-    //     txn_expiration,
-    //     signer,
-    //     payload,
-    //     sender_address,
-    //     sender_sequence_number,
-    //     max_gas_amount,
-    //     gas_unit_price,
-    //     gas_currency_code, // for compatibility with UTC's timestamp.
-    // )?;
+
+    let keypair = KeyPair::from(private_key );
+    let signer: Box<&dyn TransactionSigner> = Box::new(&keypair);
+    // sign the transaction script
+    let txn = create_user_txn(
+        signer,
+        TransactionPayload::Script(script),
+        address,
+        sequence_number,
+        0,
+        0,
+        "GAS".parse()?,
+        50000, // for compatibility with UTC's timestamp.
+    )?;
 
     // // Submit the transaction with libra_client
     // client.submit_transaction(
-    //     Some(&mut sender_account_data), 
+    //     Some(&mut sender_account_data),
     //     txn
     // )?;
 
