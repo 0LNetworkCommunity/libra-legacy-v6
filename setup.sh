52d0a125ae73feb037e2b708b873d60ec7625994
root <1364012+lpgeiger@users.noreply.github.com>
2020-08-12 17:10:21 +0000

    move fixtures to own folder, my_configs renamed validator_utils, makefile assumes home path is ~/my_configs/

--- setup.sh
+++ setup.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 # targeting ububtu
-
 apt update
-apt install -y git vim build-essential cmake clang llvm libgmp-dev secure-delete
+apt install -y git vim zip build-essential cmake clang llvm libgmp-dev secure-delete
 curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain nightly -y
 source $HOME/.cargo/env
+
+if test -d ~/my_configs/; then
+    zip -r ~/my_configs_bak.zip ~/my_configs/*
+else
+    mkdir ~/my_configs;
+    cp ~/libra/validator_utils/* ~/my_configs;
+fi 
+
+
 
 echo "Enter ssh id_rsa key for github (ctrl+d when done)"
 private_key=$(cat)
