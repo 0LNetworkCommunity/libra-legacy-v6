4fb4fbec864d73f21194fc648d1e676f96caafc2
Keerthi Nelaturu <nelaturuk@gmail.com>
2020-08-13 11:26:44 -0400

    Adding command for libra swarm

--- miner/tests/integration.rs
+++ miner/tests/integration.rs
@@ -1,9 +1,29 @@
 #![forbid(unsafe_code)]
-use std::process::Command;
+use std::process::{Command, Stdio};
+use std::{thread, time};
 #[test]
 pub fn test_command() {
-    let mut echo_hello = Command::new("sh");
-    echo_hello.arg("-c")
-            .arg("echo hello");
-    let hello_1 = echo_hello.output().expect("failed to execute process");
+    std::env::set_var("RUST_LOG", "debug");
+    let mut echo_swarm = Command::new("cargo");
+    echo_swarm.current_dir("../");
+    echo_swarm.arg("run")
+            .arg("-p").arg("libra-swarm")
+            .arg("--").arg("-n").arg("4") 
+            .arg("-l").arg("-c").arg("./saved_logs");
+    //let hello_1 = echo_swarm.output().expect("failed to execute process");
+    echo_swarm.stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .spawn()
+            .unwrap();
+    
+    let mut echo_miner = Command::new("cargo");
+    echo_miner.arg("run")
+            .arg("submit");
+    echo_miner.stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .spawn()
+            .unwrap();
+    // thread::sleep(time::Duration::from_millis(60000));
+    // println!("{:?}", hello_1);
+    // println!("{:?}", hello_1.status.success());
 }
