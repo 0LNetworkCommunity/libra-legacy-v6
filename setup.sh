1d0e2c569210c2fd54a9fe71a7799afcb53cbd07
root <1364012+lpgeiger@users.noreply.github.com>
2020-08-13 18:42:28 +0000

    setup.sh

--- setup.sh
+++ setup.sh
@@ -6,11 +6,13 @@ apt update
 apt install -y git vim zip jq build-essential cmake clang llvm libgmp-dev secure-delete
 curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain nightly -y
 export "$HOME/.cargo/bin:$PATH"
+#record-query, tool for parsing config files like TOML and JSON
 curl -LSfs https://japaric.github.io/trust/install.sh | sh -s -- --git dflemstr/rq
 
 
 mkdir ~/node_data;
-cp ~/libra/validator_utils/* ~/node_data;
+mkdir ~/node_data/blocks;
+cp ~/libra/validator_utils/Makefile ~/node_data;
 
 echo "Enter ssh id_rsa key for github (ctrl+d when done)"
 private_key=$(cat)
