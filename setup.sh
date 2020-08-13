c514389e4f2d57d5d06cff49023ebdd035bc6a09
root <1364012+lpgeiger@users.noreply.github.com>
2020-08-13 17:07:48 +0000

    makefile to read from miner.toml

--- setup.sh
+++ setup.sh
@@ -5,21 +5,16 @@ export NODE_ENV=prod
 apt update
 apt install -y git vim zip build-essential cmake clang llvm libgmp-dev secure-delete
 curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain nightly -y
-source $HOME/.cargo/env
+export "$HOME/.cargo/bin:$PATH"
+cargo install record-query
 
-if test -d ~/node_data/; then
-    zip -r ~/node_data.bak.zip ~/node_data/*
-else
-    mkdir ~/node_data;
-    cp ~/libra/validator_utils/* ~/node_data;
-fi 
+mkdir ~/node_data;
+cp ~/libra/validator_utils/* ~/node_data;
 
+echo "Enter ssh id_rsa key for github (ctrl+d when done)"
+private_key=$(cat)
+echo "$private_key" > ~/.ssh/id_rsa
+chmod 400 ~/.ssh/id_rsa
+ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub
 
-
-# echo "Enter ssh id_rsa key for github (ctrl+d when done)"
-# private_key=$(cat)
-# echo $private_key > ~/.ssh/id_rsa
-# chmod 400 ~/.ssh/id_rsa
-# ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub
-
-# git clone git@github.com:OLSF/libra.git
+git clone git@github.com:OLSF/libra.git
