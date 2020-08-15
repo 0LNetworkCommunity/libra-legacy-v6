e1ff98e8c69c5c6ba666c58cb1bb7de3caaf75f0
root <1364012+lpgeiger@users.noreply.github.com>
2020-08-15 01:32:43 +0000

    integration test

--- miner/tests/integration.rs
+++ miner/tests/integration.rs
@@ -1,9 +1,30 @@
 #![forbid(unsafe_code)]
 use std::process::{Command, Stdio};
 use wait_timeout::ChildExt;
-use std::time::Duration;
+use std::{path::PathBuf, time::Duration, fs};
 #[test]
-pub fn test_command() {
+pub fn integration() {
+
+    // PREPARE FIXTURES
+    // the transactions will always abort if the fixtures are incorrect.
+    // in swarm, all validators in genesis used NodeConfig.defaul() preimage and proofs.
+    // these are equivalent to fixtures/block_0.json.test.alice 
+    // for the test to work:
+
+    // the miner needs to start producing block_1.json. If block_1.json is not successful, then block_2 cannot be either, because it depends on certain on-chain state from block_1 correct submission.
+    
+    // remove all files in miner/blocks/
+    let blocks_dir = PathBuf::from("./blocks/");
+
+    if blocks_dir.exists() {
+        fs::remove_dir_all(&blocks_dir).unwrap();
+    } else {
+        fs::create_dir(&blocks_dir).unwrap();
+    }
+    // copy fixtures/block_0.json.test.alice -> blocks/block_0.json
+    fs::copy("../fixtures/block_0.json.test.alice", blocks_dir.join("block_0.json"));
+    
+    // TODO: Assert that block_0.json is in blocks folder.
     std::env::set_var("RUST_LOG", "debug");
     let mut echo_swarm = Command::new("cargo");
     echo_swarm.current_dir("../");
@@ -16,21 +37,28 @@ pub fn test_command() {
                 .spawn();
 
     match cmd {
-        Ok(mut child) => {
-            let timeout = Duration::from_secs(540);
-    
-            match child.wait_timeout(timeout) {
+        // Swarm has started
+        Ok(mut swarm_child) => {
+
+
+            // what to do at timeout.
+            let timeout = Duration::from_secs(30);
+
+            match swarm_child.wait_timeout(timeout) {
                 Ok(Some(status)) => println!("Exited with status {}", status),
                 Ok(None) => {
-                        println!("timeout, process is still alive");
-
-                        let mut echo_miner = Command::new("cargo");
-                        echo_miner.arg("run")
-                                .arg("submit");
-                        echo_miner.stdout(Stdio::inherit())
-                                .stderr(Stdio::inherit())
-                                .spawn().unwrap();
-                        ()           
+                    println!("Process is still alive, starting integration test");
+                    let mut echo_miner = Command::new("cargo");
+                    echo_miner.arg("run")
+                            .arg("swarm");
+                    echo_miner.stdout(Stdio::inherit())
+                            .stderr(Stdio::inherit())
+                            .spawn().unwrap();
+                    // TODO: get output and evaluate with assert
+                    // assert_eq!()
+                    
+                    swarm_child.kill().unwrap();
+                    // echo_swarm.kill().unwrap();
                 } ,
                 Err(e) => println!("Error waiting: {}", e),
             }
