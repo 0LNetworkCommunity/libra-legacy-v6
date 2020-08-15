7633c6fc8f3aee013acf8b2a67a7ffc7c2a29d43
root <1364012+lpgeiger@users.noreply.github.com>
2020-08-15 22:48:12 +0000

    WIP integration test

--- miner/tests/integration.rs
+++ miner/tests/integration.rs
@@ -1,7 +1,16 @@
 #![forbid(unsafe_code)]
-use std::process::{Command, Stdio};
 use wait_timeout::ChildExt;
-use std::{path::PathBuf, time::{self, Duration}, fs, thread};
+use std::{
+    path::{PathBuf, Path},
+    time::{self, Duration},
+    fs,
+    thread,
+    io::{BufRead, BufReader},
+    process::{Command, Stdio},
+};
+use anyhow::{bail, Error};
+use regex::Regex;
+
 #[test]
 pub fn integration() {
 
@@ -37,16 +46,35 @@ pub fn integration() {
             .arg("-p").arg("libra-swarm")
             .arg("--").arg("-n").arg("1") 
             .arg("-l").arg("-c").arg("saved_logs");
-    let cmd = echo_swarm.stdout(Stdio::inherit())
-                .stderr(Stdio::inherit())
+    let cmd = echo_swarm.stdout(Stdio::piped())
+                .stderr(Stdio::piped())
                 .spawn();
 
     match cmd {
         // Swarm has started
         Ok(mut swarm_child) => {
             // need to wait for swarm to start-up before we have the configs needed to connect to it.
-            let wait_for_swarm = Duration::from_secs(30);
-            thread::sleep(wait_for_swarm);
+            // let wait_for_swarm = Duration::from_secs(30);
+            // thread::sleep(wait_for_swarm);
+
+            let pattern = Regex::new(r"(?x)
+            (Successfully launched Swarm)").unwrap();
+
+
+            let output = swarm_child.wait_with_output().as_ref().unwrap();
+
+            let out = BufReader::new(&*output.stdout);
+
+            let is_ready = out.lines()
+            .any(|line| pattern.captures(&line.as_ref().unwrap()).is_some());
+
+
+            // out.lines()
+            // .filter(|line| pattern.captures(&line.as_ref().unwrap()).is_some())
+            // .for_each(|line| {
+            // println!("out: {:?}", line);
+            // });
+
 
             let mut echo_miner = Command::new("cargo");
             echo_miner.arg("run")
@@ -75,4 +103,100 @@ pub fn integration() {
         }
         Err(err) => println!("Process did not even start: {}", err)
     }
+}
+
+#[test]
+pub fn dir () {
+    assert_eq!( block_until_swarm_ready(), true);
+}
+
+
+pub fn block_until_swarm_ready () -> bool {
+    let swarm_configs_path = Path::new("../saved_logs/");
+    let mut timeout = 100;
+    let one_second = time::Duration::from_secs(1);
+
+    loop {
+        if timeout == 0 { 
+            return false
+        }
+        if Path::new("../saved_logs/").exists() {
+            return true
+        }
+
+        thread::sleep(one_second);
+        timeout -= 1;
+    }
+}
+
+#[test]
+pub fn test_echo () {
+   echo();
+}
+
+
+
+
+#[derive(PartialEq, Default, Clone, Debug)]
+struct Commit {
+    hash: String,
+    message: String,
+}
+
+// fn echo()  {
+//     let child = Command::new("git").arg("log").arg("--oneline").spawn().unwrap();
+
+//     // if !child.success() {
+//     //     panic!("Command executed with failing error code");
+//     // }
+
+//     let pattern = Regex::new(r"(?x)
+//                                 ([0-9a-fA-F]+) # commit hash
+//                                 (.*)           # The commit message").unwrap();
+
+//     let reader = BufReader::new(child.stdout.unwrap());
+
+//     reader
+//         .lines()
+//         .filter_map(|line| {
+//            pattern.captures(&line)
+//         })
+//         .for_each(|line| {
+//             println!("{:?}", line)
+//         });
+
+//     // let git = String::from_utf8(output.stdout).unwrap()
+//     // .lines()
+//     // .for_each(|x| println!("line {:?}\n", x));
+//     // dbg!(git);
+// }
+
+
+
+fn echo() -> Result<bool, Error> {
+    let child = Command::new("git").arg("log").arg("--oneline")
+    .stdout(Stdio::piped())
+    .spawn()?;
+
+    // if !output.status.success() {
+    //     bail!("Command executed with failing error code");
+    // }
+
+    let pattern = Regex::new(r"(?x)
+                               (competition) # commit hash
+                               (.*)           # The commit message")?;
+
+    
+    let output = child.wait_with_output().unwrap();
+
+    let out = BufReader::new(&*output.stdout);
+
+    out.lines()
+    .filter(|line| pattern.captures(&line.as_ref().unwrap()).is_some())
+    .for_each(|line| {
+        println!("out: {:?}", line);
+    });
+
+    Ok(false)
+
 }
