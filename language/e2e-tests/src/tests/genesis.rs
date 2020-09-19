2e37a6c99d39dacb48247aa440191765447ceae5
liangping <18786721@qq.com>
2020-09-19 09:46:06 +0800

    add testcase to execute upgrade stdlib

--- language/e2e-tests/src/tests/genesis.rs
+++ language/e2e-tests/src/tests/genesis.rs
@@ -2,12 +2,28 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::{data_store::GENESIS_CHANGE_SET, executor::FakeExecutor};
-use libra_types::transaction::Transaction;
+use libra_types::transaction::{Transaction, TransactionPayload};
+
+use stdlib::{transaction_scripts::StdlibScript, StdLibOptions};
 
 #[test]
 fn execute_genesis_write_set() {
     let executor = FakeExecutor::no_genesis();
     let txn = Transaction::WaypointWriteSet(GENESIS_CHANGE_SET.clone());
+    let mut output = executor.execute_transaction_block(vec![txn]).unwrap();
+
+    // Executing the genesis transaction should succeed
+    assert_eq!(output.len(), 1);
+    assert!(!output.pop().unwrap().status().is_discarded())
+}
+
+#[test]
+fn execute_genesis_stdlib() {
+    let executor = FakeExecutor::no_genesis();
+    //let txn = Transaction::WaypointWriteSet(GENESIS_CHANGE_SET.clone());
+    let txn = Transaction::WaypointWriteSet(
+        transaction_builder::encode_stdlib_upgrade_transaction(StdLibOptions::Fresh)
+    );
     let mut output = executor.execute_transaction_block(vec![txn]).unwrap();
 
     // Executing the genesis transaction should succeed
