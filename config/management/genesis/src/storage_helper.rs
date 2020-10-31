8697f9d7cd4c70f44cb3820c33b5159b452ac024
root <root>
2020-10-31 18:58:28 +0000

    make owner and operator keys separate from others

--- config/management/genesis/src/storage_helper.rs
+++ config/management/genesis/src/storage_helper.rs
@@ -90,19 +90,19 @@ impl StorageHelper {
             .import_private_key(OWNER_KEY, keys.child_1_operator.get_private_key())
             .unwrap();
         storage
-            .import_private_key(OPERATOR_KEY, keys.child_0_owner.get_private_key())
+            .import_private_key(OPERATOR_KEY, keys.child_1_operator.get_private_key())
             .unwrap();
         storage
-            .import_private_key(VALIDATOR_NETWORK_KEY, keys.child_0_owner.get_private_key())
+            .import_private_key(VALIDATOR_NETWORK_KEY, keys.child_2_val_network.get_private_key())
             .unwrap();
         storage
-            .import_private_key(FULLNODE_NETWORK_KEY, keys.child_0_owner.get_private_key())
+            .import_private_key(FULLNODE_NETWORK_KEY, keys.child_2_val_network.get_private_key())
             .unwrap();
         storage
-            .import_private_key(CONSENSUS_KEY, keys.child_0_owner.get_private_key())
+            .import_private_key(CONSENSUS_KEY, keys.child_2_val_network.get_private_key())
             .unwrap();
         storage
-            .import_private_key(EXECUTION_KEY, keys.child_0_owner.get_private_key())
+            .import_private_key(EXECUTION_KEY, keys.child_2_val_network.get_private_key())
             .unwrap();
         storage
             .set(SAFETY_DATA, SafetyData::new(0, 0, 0, None))
