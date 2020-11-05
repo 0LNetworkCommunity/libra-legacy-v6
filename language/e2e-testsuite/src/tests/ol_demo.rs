78e990e9344077354d004acd7ed997c8b2678094
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2020-11-05 01:45:34 +0000

    WIP miner commit test

--- language/e2e-testsuite/src/tests/ol_demo.rs
+++ language/e2e-testsuite/src/tests/ol_demo.rs
@@ -3,24 +3,27 @@
 
 use language_e2e_tests::{
     account::{Account},
-    executor::FakeExecutor,
-    setup_0L::demo_tx,
+    executor::FakeExecutor
 };
 use libra_types::{transaction::TransactionStatus, vm_status::KeptVMStatus};
+use transaction_builder::encode_minerstate_commit_script;
+
 
 #[test]
 fn demo() {
-    let mut executor = FakeExecutor::from_genesis_file();
-    // use system account
-    let sender = Account::new_libra_root();
-
-    // uses a helper in e2e test infrastructure
-    let txn = demo_tx(
-        &sender,
-        1,
-    );
-
-    // execute transaction
+  let mut executor = FakeExecutor::from_genesis_file();
+  // use system account
+  let sender = Account::new_libra_root();
+  let hello_world= 100u64;
+  let seq_num = 1;
+  let script = transaction_builder::encode_demo_e2e_script(hello_world);
+  let txn = sender
+      .transaction()
+      .script(script)
+      .sequence_number(seq_num)
+      .sign();
+  
+        // execute transaction
     let output = executor.execute_transaction(txn);
     assert_eq!(
         output.status(),
@@ -28,14 +31,16 @@ fn demo() {
     );
     executor.apply_write_set(output.write_set());
 
-    // check that numbers in stored DB are correct
-    // let updated_sender = executor
-    //     .read_account_resource(&sender)
-    //     .expect("sender must exist");
-
-    // let updated_receiver_balance = executor
-    //     .read_balance_resource(&new_account, account::coin1_tmp_currency_code())
-    //     .expect("receiver balance must exist");
-    // assert_eq!(initial_amount, updated_receiver_balance.coin());
-    // assert_eq!(1, updated_sender.sequence_number());
 }
+// #[test]
+// fn miner_commit () {
+//   encode_minerstate_commit_script(
+//       challenge,
+//       solution,
+//     );
+//     sender
+//         .transaction()
+//         .script(script)
+//         .sequence_number(seq_num)
+//         .sign()
+// }
