ccf153d8c9f7bb645cf083e0906f9616ceec6259
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2020-11-07 20:42:09 +0000

    WIP config proofs in genesis

--- config/global-constants/src/lib.rs
+++ config/global-constants/src/lib.rs
@@ -22,7 +22,10 @@ pub const VALIDATOR_NETWORK_ADDRESS_KEYS: &str = "validator_network_address_keys
 pub const VALIDATOR_NETWORK_KEY: &str = "validator_network";
 
 // 0L
+// TODO remove fullnode
 pub const FULLNODE_PEER_ID: &str = "fullnode_peer_id";
+pub const OPERATOR_PROOF_OF_WORK_PREIMAGE: &str = "operator_pow_preimage";
+pub const OPERATOR_PROOF_OF_WORK_PROOF: &str = "operator_pow_proof";
 
 /// Definitions of global data items (e.g., as held in secure storage)
 pub const SAFETY_DATA: &str = "safety_data";
---
+++ config/management/genesis/src/mining.rs
@@ -0,0 +1,63 @@
+use libra_management::{
+    error::Error,
+    secure_backend::{SecureBackend, SharedBackend},
+};
+use libra_secure_storage::{Storage};
+use miner::block::Block;
+use std::convert::TryInto;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+pub struct Mining {
+    #[structopt(long, short)]
+    pub path_to_genesis_pow: PathBuf,
+    #[structopt(flatten)]
+    shared_backend: SharedBackend,
+}
+
+impl Mining {
+    pub fn execute(self) -> Result<String, Error> {
+        let config = self
+            .config
+            .load()?
+            .override_shared_backend(&self.shared_backend.shared_backend)?;
+
+        let (preimage, proof) = Block::get_genesis_tx_data(&self.path_to_genesis_pow)
+            .map_err(|e| Error::UnexpectedError(e.to_string()))?;
+        let preimage = hex::encode(preimage);
+        let proof = hex::encode(proof);
+
+        let mut shared_storage = config.shared_backend();
+        shared_storage.set(libra_global_constants::OPERATOR_PROOF_OF_WORK_PREIMAGE, preimage)?;
+        shared_storage.set(libra_global_constants::OPERATOR_PROOF_OF_WORK_PROOF, proof)?;
+
+
+        // let mut remote: Box<dyn Storage> = self.backend.backend.try_into()?;
+        // remote
+        //     .available()
+        //     .map_err(|e| Error::RemoteStorageUnavailable(e.to_string()))?;
+
+        // remote
+        //     .set(
+        //         libra_global_constants::OPERATOR_PROOF_OF_WORK_PREIMAGE,
+        //         preimage,
+        //     )
+        //     .map_err(|e| {
+        //         Error::RemoteStorageWriteError(
+        //             libra_global_constants::OPERATOR_PROOF_OF_WORK_PREIMAGE,
+        //             e.to_string(),
+        //         )
+        //     })?;
+        // remote
+        //     .set(libra_global_constants::OPERATOR_PROOF_OF_WORK_PROOF, proof)
+        //     .map_err(|e| {
+        //         Error::RemoteStorageWriteError(
+        //             libra_global_constants::OPERATOR_PROOF_OF_WORK_PROOF,
+        //             e.to_string(),
+        //         )
+        //     })?;
+
+        Ok("Sent Proof".to_string())
+    }
+}
