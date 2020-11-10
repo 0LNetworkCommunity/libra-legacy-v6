931b5e7193d8ed3c2644111749fa90a75c9dba5a
root <root>
2020-11-10 23:47:09 +0000

    patch

--- miner/src/commands.rs
+++ miner/src/commands.rs
@@ -68,6 +68,8 @@ impl Configurable<MinerConfig> for MinerCmd {
         // Check if the config file exists, and if it does not, ignore it.
         // If you'd like for a missing configuration file to be a hard error
         // instead, always return `Some(CONFIG_FILE)` here.
+        let filename = PathBuf::from(CONFIG_FILE);
+
         if filename.exists() {
             Some(filename)
         } else {
