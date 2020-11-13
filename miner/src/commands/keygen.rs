b279bb932afee66b3d2f1d4dc95790bb87de3167
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2020-11-13 00:50:24 +0000

    keygen creates files for miner

--- miner/src/commands/keygen.rs
+++ miner/src/commands/keygen.rs
@@ -2,9 +2,9 @@
 
 #![allow(clippy::never_loop)]
 use abscissa_core::{Command, Options, Runnable};
-use libra_wallet::{WalletLibrary};
+use libra_wallet::WalletLibrary;
 use crate::config;
-use crate::commands::{CONFIG_FILE};
+use crate::commands::CONFIG_FILE;
 use libra_global_constants::NODE_HOME;
 use toml;
 use std::{net::Ipv4Addr, fs, io::Write};
@@ -17,26 +17,29 @@ pub struct KeygenCmd {}
 impl Runnable for KeygenCmd {
     /// Print version message
     fn run(&self) {
+        println!("Miner not initialized, creating configs at {}", NODE_HOME);
         let mut miner_configs = config::MinerConfig::default();
         miner_configs.workspace.node_home = dirs::home_dir().unwrap();
         miner_configs.workspace.node_home.push(NODE_HOME);
-        dbg!(&miner_configs.workspace.node_home);
         fs::create_dir_all(&miner_configs.workspace.node_home).unwrap();
 
-        println!("Enter Miner configurations");
+        println!("Enter configs...");
+        // Set up github token
         let mut rl = Editor::<()>::new();
-        let readline = rl.readline("IP address of node: ").expect("Must enter an ip address, eg. 0.0.0.0");
-        let ip_address: Ipv4Addr = readline.parse().expect("Could not parse IP address");
-        let get_statement = rl.readline("Your statement:").expect("Please enter a fun statement to go into genesis proof.");
 
-
-        let get_gh_token = rl.readline("Github Token:").expect("Please enter a fun statement to go into genesis proof.");
-        
+        let get_gh_token = rl.readline("Github Token: ").expect("Please enter a fun statement to go into genesis proof.");
         let token_path = miner_configs.workspace.node_home.join("github_token.txt");
-        dbg!(&token_path);
         let file = fs::File::create(token_path.to_str().unwrap());
         file.unwrap().write(&get_gh_token.as_bytes())
-            .expect("Could not write github_token.txt file");
+        .expect("Could not write github_token.txt file");
+
+
+        let readline = rl.readline("IP address of node: ").expect("Must enter an ip address, eg. 0.0.0.0");
+        let ip_address: Ipv4Addr = readline.parse().expect("Could not parse IP address");
+        
+        let get_statement = rl.readline("Your statement: ").expect("Please enter a fun statement to go into genesis proof.");
+
+
         
         // Generate new keys
         let mut wallet = WalletLibrary::new();
