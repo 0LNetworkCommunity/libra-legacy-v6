567f3c534a059040fd5d98b655fa1027c8322466
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2020-11-14 18:35:34 +0000

    patch miner genesis

--- miner/src/commands/genesis.rs
+++ miner/src/commands/genesis.rs
@@ -1,6 +1,6 @@
 //! `start` subcommand - example of how to write a subcommand
 
-use crate::{block::Block, block::ValConfigs, node_keys::KeyScheme};
+use crate::{block::Block, block::ValConfigs, node_keys::KeyScheme, block::build_block};
 use crate::config::MinerConfig;
 use crate::prelude::*;
 use std::{fs, path::PathBuf};
@@ -24,6 +24,8 @@ impl Runnable for GenesisCmd {
         let miner_configs = app_config();
         println!("Enter your 0L mnemonic:");
         let mnemonic_string = rpassword::read_password_from_tty(Some("\u{1F511} ")).unwrap();
+
+        build_block::mine_genesis(&miner_configs);
 
         // Create val_init.json file.
         let keys = KeyScheme::new_from_mnemonic(mnemonic_string);
