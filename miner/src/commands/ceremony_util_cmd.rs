387fbf0a81e972e22f136de9e737363f19d4476f
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2020-11-13 21:52:23 -0500

    cleanup

--- miner/src/commands/ceremony_util_cmd.rs
+++ miner/src/commands/ceremony_util_cmd.rs
@@ -54,6 +54,8 @@ impl Runnable for CeremonyUtilCmd {
             .expect("Could not write toml file");    
 
         println!("\nMining Proof of Work, block_0.json");
+        println!("\nThis will take about 10 mins");
+
 
         build_block::mine_genesis(&miner_configs);
         println!("\nWelcome");
