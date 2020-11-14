c4215a93d020b66eecce222d354a31db1b6465fe
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2020-11-13 21:25:41 -0500

    cleanup

--- miner/src/commands/keygen_cmd.rs
+++ miner/src/commands/keygen_cmd.rs
@@ -2,15 +2,7 @@
 
 #![allow(clippy::never_loop)]
 use abscissa_core::{Command, Options, Runnable};
-use libra_wallet::WalletLibrary;
-use crate::{node_keys::KeyScheme, config};
-use crate::commands::CONFIG_FILE;
 use crate::keygen::keygen;
-use libra_global_constants::NODE_HOME;
-use toml;
-use std::{net::Ipv4Addr, fs, io::Write};
-use rustyline::Editor;
-use config::MinerConfig;
 
 /// `version` subcommand
 #[derive(Command, Debug, Default, Options)]
