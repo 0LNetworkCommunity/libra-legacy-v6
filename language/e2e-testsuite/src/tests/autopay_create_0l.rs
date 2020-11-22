0aa5f53b474fb5e6c19d3144ad4fade025c03162
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2020-11-22 23:24:12 +0000

    autopay create e2e test passing

--- language/e2e-testsuite/src/tests/autopay_create_0l.rs
+++ language/e2e-testsuite/src/tests/autopay_create_0l.rs
@@ -1,10 +1,7 @@
 // Copyright (c) 0lsf
 // SPDX-License-Identifier: Apache-2.0
 
-use language_e2e_tests::{
-    account::{Account},
-    executor::FakeExecutor
-};
+use language_e2e_tests::{account::AccountData, executor::FakeExecutor};
 use libra_types::{transaction::TransactionStatus, vm_status::KeptVMStatus};
 use transaction_builder;
 
@@ -13,23 +10,37 @@ use transaction_builder;
 fn autopay_create_test() {
   let mut executor = FakeExecutor::from_genesis_file();
 
-  let seq_num = 1;
-  let sender = Account::new_libra_root();
+  let sender = AccountData::new(1_000_000, 1);
+  executor.add_account_data(&sender);
+  let mut seq_num = 1;
+  let script = transaction_builder::encode_enable_autopay_tx_script();
+  let txn_enable = sender.clone().into_account()
+    .transaction()
+    .script(script)
+    .sequence_number(seq_num)
+    .sign();
 
+  // execute transaction
+  let output = executor.execute_and_apply(txn_enable);
+  assert_eq!(
+    output.status(),
+    &TransactionStatus::Keep(KeptVMStatus::Executed)
+  );
+
+  seq_num = 2;
 
   let script = transaction_builder::encode_autopay_create_instruction_tx_script();
-  let txn = sender
-      .transaction()
-      .script(script)
-      .sequence_number(seq_num)
-      .sign();
-  
-        // execute transaction
-    let output = executor.execute_transaction(txn);
-    assert_eq!(
-        output.status(),
-        &TransactionStatus::Keep(KeptVMStatus::Executed)
-    );
-    executor.apply_write_set(output.write_set());
+  let txn_create = sender.into_account()
+    .transaction()
+    .script(script)
+    .sequence_number(seq_num)
+    .sign();
 
+//         // execute transaction
+    let output = executor.execute_transaction(txn_create);
+    assert_eq!(
+      output.status(),
+      &TransactionStatus::Keep(KeptVMStatus::Executed)
+    );
+//     executor.apply_write_set(output.write_set());
 }
