20d0330ee92de18a0726ce42e29a8d1ff60ad7ce
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2020-12-27 16:41:02 +0000

    reverse changes to transaction builder

--- language/transaction-builder/generator/src/rust.rs
+++ language/transaction-builder/generator/src/rust.rs
@@ -364,7 +364,6 @@ static SCRIPT_DECODER_MAP: once_cell::sync::Lazy<DecoderMap> = once_cell::sync::
             Address => ("Address", "Some(value)".to_string()),
             Vector(type_tag) => match type_tag.as_ref() {
                 U8 => ("U8Vector", "Some(value)".to_string()),
-                Address => ("Address", "Some(value)".to_string()),
                 _ => common::type_not_allowed(type_tag),
             },
             Struct(_) | Signer => common::type_not_allowed(type_tag),
@@ -449,8 +448,7 @@ fn decode_{}_argument(arg: TransactionArgument) -> Option<{}> {{
                     } else {
                         "Bytes".into()
                     }
-                },
-                Address => "AccountAddress".into(),
+                }
                 _ => common::type_not_allowed(type_tag),
             },
 
@@ -468,7 +466,6 @@ fn decode_{}_argument(arg: TransactionArgument) -> Option<{}> {{
             Address => format!("TransactionArgument::Address({})", name),
             Vector(type_tag) => match type_tag.as_ref() {
                 U8 => format!("TransactionArgument::U8Vector({})", name),
-                Address => format!("TransactionArgument::Address({})", name),
                 _ => common::type_not_allowed(type_tag),
             },
 
