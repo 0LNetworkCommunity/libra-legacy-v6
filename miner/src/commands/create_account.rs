07981c113a8615bb8fdd8771a6236ca121d2bade
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2021-01-15 20:36:51 +0000

    cleanup

--- miner/src/commands/create_account.rs
+++ miner/src/commands/create_account.rs
@@ -16,7 +16,7 @@ pub struct CreateCmd {
     #[options(help = "regenerates account manifest from mnemonic")]
     fix: bool,
     #[options(help = "creates a validator account")]
-    is_validator: bool,
+    validator: bool,
     #[options(help = "use an existing block_0.json file and skip mining")]
     block_zero: Option<PathBuf>,
 }
@@ -29,47 +29,47 @@ impl Runnable for CreateCmd {
         if self.check {
             check(path);
         } else {
-            create(path, self.fix, self.is_validator, &self.block_zero);
+            create(path, self.fix, self.validator, &self.block_zero);
         }
     }
 }
 
 fn create(path: PathBuf, is_fix: bool, is_validator: bool, block_zero: &Option<PathBuf>) {
-        let mut miner_configs = config::MinerConfig::default();
-        let keys;
+    let mut miner_configs = config::MinerConfig::default();
+    let keys;
 
-        if is_fix {
-            let mnemonic_string = rpassword::read_password_from_tty(Some("\u{1F511} ")).unwrap();
-            let (authkey, account, wallet) = keygen::get_account_from_mnem(mnemonic_string);
+    if is_fix {
+        let mnemonic_string = rpassword::read_password_from_tty(Some("\u{1F511} ")).unwrap();
+        let (authkey, account, wallet) = keygen::get_account_from_mnem(mnemonic_string);
 
-            miner_configs.profile.auth_key = authkey.to_string();
-            miner_configs.profile.account = account;
-            keys = KeyScheme::new(wallet);
-        } else {
-            let (authkey, account, wallet) = keygen::keygen();
-            miner_configs.profile.auth_key = authkey.to_string();
-            miner_configs.profile.account = account;
-            keys = KeyScheme::new(wallet);
-        }
+        miner_configs.profile.auth_key = authkey.to_string();
+        miner_configs.profile.account = account;
+        keys = KeyScheme::new(wallet);
+    } else {
+        let (authkey, account, wallet) = keygen::keygen();
+        miner_configs.profile.auth_key = authkey.to_string();
+        miner_configs.profile.account = account;
+        keys = KeyScheme::new(wallet);
+    }
 
-        let block;
-        if let Some(block_path) = block_zero {
-            block = build_block::parse_block_file(block_path.to_owned());
-        } else {
-            block = build_block::write_genesis(&miner_configs);
-        }
+    let block;
+    if let Some(block_path) = block_zero {
+        block = build_block::parse_block_file(block_path.to_owned());
+    } else {
+        block = build_block::write_genesis(&miner_configs);
+    }
 
-        if is_validator {
-            account::ValConfigs::new(
-                block,
-                keys,  
-                miner_configs.profile.ip.to_string()
-            ).create_manifest(path);
+    if is_validator {
+        account::ValConfigs::new(
+            block,
+            keys,  
+            miner_configs.profile.ip.to_string()
+        ).create_manifest(path);
 
-        } else {
-            account::UserConfigs::new(block)
-            .create_manifest(path);
-        } 
+    } else {
+        account::UserConfigs::new(block)
+        .create_manifest(path);
+    } 
 }
 
 /// Checks the format of the account manifest, including vdf proof
