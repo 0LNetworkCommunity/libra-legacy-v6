9d971bc40d8c2a997a84fd16686f46c2d128e26b
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2021-02-02 18:59:30 +0000

    patch genesis add proofs

--- config/management/genesis/src/mining.rs
+++ config/management/genesis/src/mining.rs
@@ -29,43 +29,17 @@ impl Mining {
         shared_storage.set(libra_global_constants::PROOF_OF_WORK_PREIMAGE, preimage)?;
         shared_storage.set(libra_global_constants::PROOF_OF_WORK_PROOF, proof)?;
 
-
-        // let mut remote: Box<dyn Storage> = self.backend.backend.try_into()?;
-        // remote
-        //     .available()
-        //     .map_err(|e| Error::RemoteStorageUnavailable(e.to_string()))?;
-
-        // remote
-        //     .set(
-        //         libra_global_constants::OPERATOR_PROOF_OF_WORK_PREIMAGE,
-        //         preimage,
-        //     )
-        //     .map_err(|e| {
-        //         Error::RemoteStorageWriteError(
-        //             libra_global_constants::OPERATOR_PROOF_OF_WORK_PREIMAGE,
-        //             e.to_string(),
-        //         )
-        //     })?;
-        // remote
-        //     .set(libra_global_constants::OPERATOR_PROOF_OF_WORK_PROOF, proof)
-        //     .map_err(|e| {
-        //         Error::RemoteStorageWriteError(
-        //             libra_global_constants::OPERATOR_PROOF_OF_WORK_PROOF,
-        //             e.to_string(),
-        //         )
-        //     })?;
-
         Ok("Sent Proof".to_string())
     }
 }
 
-pub fn get_genesis_tx_data(path: &std::path::PathBuf) -> Result<(String,String),std::io::Error> {
+pub fn get_genesis_tx_data(path: &std::path::PathBuf) -> Result<(String, String),std::io::Error> {
         let file = std::fs::File::open(path)?;
         let reader = std::io::BufReader::new(file);
         let json: serde_json::Value = serde_json::from_reader(reader).expect("Genesis block should deserialize");
         let block = json.as_object().unwrap();
         return Ok((
             block["preimage"].as_str().unwrap().to_owned(),
-            block["preimage"].as_str().unwrap().to_owned()
+            block["proof"].as_str().unwrap().to_owned()
         ));
     }
