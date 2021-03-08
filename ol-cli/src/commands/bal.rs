cb8ac4868cf539188b4853e72d551a7482a3f47e
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2021-03-08 22:28:46 +0000

    wip account resource

--- ol-cli/src/commands/bal.rs
+++ ol-cli/src/commands/bal.rs
@@ -1,18 +1,67 @@
 //! `bal` subcommand
 
 use abscissa_core::{Command, Options, Runnable};
-use cli::{
+use cli::libra_client::LibraClient;
+use reqwest::Url;
+use libra_types::{account_address::AccountAddress, account_state::AccountState, transaction::Version, waypoint::Waypoint};
+use num_format::{Locale, ToFormattedString};
+use resource_viewer::{AnnotatedAccountStateBlob, MoveValueAnnotator, NullStateView};
+
+use anyhow::Error;
+use crate::{
+    commands::{is_address, is_authentication_key},
     libra_client::LibraClient,
-    AccountData,
-    AccountStatus
+    AccountData, AccountStatus,
+};
+use anyhow::{bail, ensure, format_err, Error, Result};
+use compiled_stdlib::StdLibOptions;
+use compiler::Compiler;
+use libra_crypto::{
+    ed25519::{Ed25519PrivateKey, Ed25519PublicKey, Ed25519Signature},
+    test_utils::KeyPair,
+};
+use libra_json_rpc_client::views::{AccountView, EventView, MetadataView, TransactionView, VMStatusView, MinerStateResourceView, OracleResourceView};
+use libra_logger::prelude::*;
+use libra_temppath::TempPath;
+use libra_types::{
+    access_path::AccessPath,
+    account_address::AccountAddress,
+    account_config::{
+        from_currency_code_string, libra_root_address, testnet_dd_account_address,
+        treasury_compliance_account_address, type_tag_for_currency_code,
+        ACCOUNT_RECEIVED_EVENT_PATH, ACCOUNT_SENT_EVENT_PATH, COIN1_NAME, LBR_NAME,
+    },
+    account_state::AccountState,
+    chain_id::ChainId,
+    ledger_info::LedgerInfoWithSignatures,
+    transaction::{
+        authenticator::AuthenticationKey,
+        helpers::{create_unsigned_txn, create_user_txn, TransactionSigner},
+        parse_transaction_argument, Module, RawTransaction, Script, SignedTransaction,
+        TransactionArgument, TransactionPayload, Version, WriteSetPayload,
+    },
+    waypoint::Waypoint,
+};
+use libra_wallet::{Mnemonic, WalletLibrary, io_utils};
+use num_traits::{
+    cast::{FromPrimitive, ToPrimitive},
+    identities::Zero,
 };
 use reqwest::Url;
-use libra_types::{
-    waypoint::Waypoint,
-    account_address::AccountAddress,
+use resource_viewer::{AnnotatedAccountStateBlob, MoveValueAnnotator, NullStateView};
+use rust_decimal::Decimal;
+use std::{
+    collections::HashMap,
+    convert::TryFrom,
+    fmt, fs,
+    io::{stdout, Write},
+    path::{Path, PathBuf},
+    process::Command,
+    str::{self, FromStr},
+    thread, time,
 };
-use num_format::{Locale, ToFormattedString};
-
+use std::fs::File;
+use std::io::Read;
 
 /// `bal` subcommand
 ///
@@ -46,5 +95,22 @@ impl Runnable for BalCmd {
         for av in account_view.unwrap().balances.iter() {
             if av.currency == "GAS" { println!("{} GAS", av.amount.to_formatted_string(&Locale::en)) }
         }
+    }
+}
+
+
+fn get_annotate_account_blob(
+    &mut self,
+    address: AccountAddress,
+) -> Result<(Option<AnnotatedAccountStateBlob>, Version)> {
+    let (blob, ver) = self.client.get_account_state_blob(address)?;
+    if let Some(account_blob) = blob {
+        let state_view = NullStateView::default();
+        let annotator = MoveValueAnnotator::new(&state_view);
+        let annotate_blob =
+            annotator.view_account_state(&AccountState::try_from(&account_blob)?)?;
+        Ok((Some(annotate_blob), ver))
+    } else {
+        Ok((None, ver))
     }
 }
