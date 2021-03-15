d780b950c8f21194309f821146bf4d2581098980
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2021-03-15 23:59:17 +0000

    wip, zip file is empty

--- ol-cli/src/restore.rs
+++ ol-cli/src/restore.rs
@@ -6,7 +6,7 @@ use reqwest::{
 };
 use serde::{Serialize, Deserialize};
 use warp::Buf;
-use std::{fs::File, io::{self, Write}, path::{Path, PathBuf}, process::Command};
+use std::{fs::{self, File}, io::{self, Write}, path::{Path, PathBuf}, process::Command};
 use crate::check;
 use crate::application::app_config;
 
@@ -31,6 +31,7 @@ env!("CARGO_PKG_NAME"),
 env!("CARGO_PKG_VERSION"),
 );
 
+#[derive(Debug)]
 pub struct Backup {
     version_number: u64,
     zip_url: String,
@@ -41,30 +42,34 @@ pub struct Backup {
 
 impl Backup {
     /// Creates a backup info instance
-    pub fn new() -> Self{
+    pub fn new() -> Self {
         let conf = app_config().to_owned();
         let (version_number, zip_url) = get_highest_epoch_zip().unwrap();
+        let restore_path = conf.home_path.join(format!("restore-{}/", version_number));
+        fs::create_dir_all(&restore_path).unwrap();
+
         Backup {
             version_number,
             zip_url,
             home_path: conf.home_path.clone(),
-            restore_path: conf.home_path.join("restore/"),
-            zip_path: conf.home_path.join(format!("restore/{}.zip", version_number))
+            restore_path: restore_path.clone(),
+            zip_path: conf.home_path.join(format!("restore-{}.zip", version_number))
         }
     }
     /// Fetch backups
     pub fn fetch_backup(&self) -> Result<(), Error> {
+        dbg!(&self);
         let mut resp = reqwest::blocking::get(&self.zip_url).expect("request failed");
         let mut out = File::create(&self.zip_path).expect("failed to create file");
         io::copy(&mut resp, &mut out).expect("failed to copy content");
 
-            let mut child = Command::new("unzip")
-                .arg("-j") // prevents an extra directory layer
-                .arg(&self.zip_path)
-                .arg("-d")
-                .arg(&self.restore_path)
-                .spawn()
-                .expect("failed to execute child");
+        let mut child = Command::new("unzip")
+            .arg("-j") // prevents an extra directory layer
+            .arg(&self.zip_path)
+            // .arg("-d")
+            // .arg(&self.restore_path)
+            .spawn()
+            .expect("failed to execute child");
 
         let ecode = child.wait()
                     .expect("failed to wait on child");
@@ -75,6 +80,15 @@ impl Backup {
 
     /// Restore Backups
     pub fn restore_backup(&self) -> Result<(), Error>{
+//         restore-epoch:
+// 	db-restore --target-db-dir ${DB_PATH} epoch-ending --epoch-ending-manifest ${ARCHIVE_PATH}/${EPOCH}/epoch_ending_${EPOCH}*/epoch_ending.manifest local-fs --dir ${ARCHIVE_PATH}/${EPOCH}
+
+// restore-transaction:
+// 	db-restore --target-db-dir ${DB_PATH} transaction --transaction-manifest ${ARCHIVE_PATH}/${EPOCH}/transaction_${EPOCH_HEIGHT}*/transaction.manifest local-fs --dir ${ARCHIVE_PATH}/${EPOCH}
+
+// restore-snapshot:
+// 	db-restore --target-db-dir ${DB_PATH} state-snapshot --state-manifest ${ARCHIVE_PATH}/${EPOCH}/state_ver_${EPOCH_HEIGHT}*/state.manifest --state-into-version ${EPOCH_HEIGHT} local-fs --dir ${ARCHIVE_PATH}/${EPOCH}
+
         Ok(())
     }
 
@@ -101,27 +115,19 @@ fn get_highest_epoch_zip() -> Result<(u64, String), Error> {
     let request_url = format!("https://api.github.com/repos/{owner}/{repo}/contents/",
                               owner = "OLSF",
                               repo = "epoch-archive");
-    println!("{}", request_url);
     let response = client.get(&request_url).send()?;
-    // let text = response.text()?;
-    // println!("{:?}", &text);
 
     let files: Vec<GithubFile> = response.json()?;
-    println!("{:?}", files);
-
     let mut filter: Vec<u64> = files.into_iter()
     .filter(|file|{
         file.file_type == "dir"
     })
     .map(|file| {
         // true
-        dbg!(&file);
         file.name.parse::<u64>().unwrap_or(0)
     })
     .collect();
-    println!("{:?}", &filter);
     filter.sort();
-    println!("{:?}", &filter);
     let highest_epoch = filter.pop().unwrap();
     // TODO: Change to new directory structure
     Ok(
