145c362bf33245873e5554cc3efef889a2ef0010
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2021-03-23 00:37:35 +0000

    server scaffold for explorer

--- ol-cli/src/server.rs
+++ ol-cli/src/server.rs
@@ -1,7 +1,6 @@
 //! server
 
-#![deny(warnings)]
-use std::{sync::Arc};
+use std::sync::Arc;
 use handlebars::Handlebars;
 use serde::Serialize;
 use serde_json::json;
@@ -11,7 +10,6 @@ use std::time::Duration;
 use tokio::time::interval;  
 use warp::{sse::ServerSentEvent, Filter};
 use crate::{check, check_runner};
-
 use std::thread;
 
 
@@ -20,11 +18,10 @@ struct WithTemplate<T: Serialize> {
     value: T,
 }
 
+// TODO: does this need to be a separate function?
 // create server-sent event
-fn sse_counter(info: check::Items) -> Result<impl ServerSentEvent, Infallible> {
-    // Ok(warp::sse::data(format!("{:#?}", serde_json::to_string(&info))));
+fn sse_check(info: check::Items) -> Result<impl ServerSentEvent, Infallible> {
     Ok(warp::sse::json(info))
-
 }
 
 fn render<T>(template: WithTemplate<T>, hbs: Arc<Handlebars<'_>>) -> impl warp::Reply
@@ -44,6 +41,7 @@ pub const TEMPLATE: &'static str = std::include_str!("../web/index.html");
 #[tokio::main]
 pub async fn main() {
 
+    // TODO: Perhaps a better way to keep the check cache fresh?
     thread::spawn(|| {
         check_runner::mon(true);
     });
@@ -60,7 +58,7 @@ pub async fn main() {
     // Create a reusable closure to render template
     let handlebars = move |with_template| render(with_template, hb.clone());
 
-    //GET /
+    //GET / (index html)
     let route = warp::get()
         .and(warp::path::end())
         .map(|| {
@@ -72,21 +70,32 @@ pub async fn main() {
         })
         .map(handlebars);
     
-    //GET ticks/
-    let ticks = warp::path("check").and(warp::get()).map(|| {
-
-        // let mut counter: u64 = 0;
-        // create server event source
+    //GET check/ (json api for check data)
+    let check = warp::path("check").and(warp::get()).map(|| {
+        // create server event source from Check object
         let event_stream = interval(Duration::from_secs(1)).map(move |_| {
             // counter += 1;
             let items = check::Items::read_cache().unwrap();
 
-            sse_counter(items)
+            sse_check(items)
         });
         // reply using server-sent events
         warp::sse::reply(event_stream)
     });
-    // TODO: Server runs monitor in background.
-    // monitor::mon();
-    warp::serve(route.or(ticks)).run(([127, 0, 0, 1], 3030)).await;
+
+    //GET explorer/ (the json api for explorer)
+    let _explorer = warp::path("explorer").and(warp::get()).map(|| {
+
+        // create server event source
+        let event_stream = interval(Duration::from_secs(1)).map(move |_| {
+            let items = check::Items::read_cache().unwrap();
+            
+            // TODO: Use a different data source for /explorer/ data.
+            sse_check(items)
+        });
+        // reply using server-sent events
+        warp::sse::reply(event_stream)
+    });
+
+    warp::serve(route.or(check)).run(([127, 0, 0, 1], 3030)).await;
 }
