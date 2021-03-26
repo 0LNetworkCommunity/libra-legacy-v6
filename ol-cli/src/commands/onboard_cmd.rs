47ae3a3e72fb0444e5511eaf595f97e52d72baee
Gökhan Şimşek <g.simsek@paralect.com>
2021-03-26 14:16:08 +0300

    Refactoring onboard cmd

--- ol-cli/src/commands/onboard_cmd.rs
+++ ol-cli/src/commands/onboard_cmd.rs
@@ -1,9 +1,8 @@
 //! `onboard-cmd` subcommand
 
-use std::{thread, time::Duration};
-
-use abscissa_core::{Command, Options, Runnable};
 use crate::transitions;
+use std::{thread, time::Duration};
+use abscissa_core::{Command, Options, Runnable};
 
 /// `onboard-cmd` subcommand
 ///
@@ -13,35 +12,37 @@ use crate::transitions;
 ///
 /// <https://docs.rs/gumdrop/>
 #[derive(Command, Debug, Options)]
-pub struct OnboardCmd {
-    /// attempt to trigger actions at every state
-    #[options(short = "t", help = "attempt to trigger actions at every state")]
+pub struct OnboardCmd {    
+    #[options(help = "advance to all next states without supervision")]
+    autopilot: bool,
+
+    #[options(help = "advance to the next state")]
+    next: bool,
+
+    #[options(help = "attempt to trigger actions at every state")]
     trigger_actions: bool,
-    // "free" arguments don't have an associated flag
-    #[options(free)]
-    free_args: Vec<String>,
 }
 
 impl Runnable for OnboardCmd {
     /// Start the application.
     fn run(&self) {
-        if self.free_args.clone().into_iter().find(|x| x == "next").is_some() {
-            let state = transitions::NodeState::init()
-            .maybe_advance(self.trigger_actions)
-            .get_state();
-            println!("Onboarding stage at exit: {:?}", state);
-
-        } 
-
-        if self.free_args.clone().into_iter().find(|x| x == "autopilot").is_some() {
+        let trigger_actions = self.trigger_actions;
+        let trigger_actions_str = format!("trigger_actions: {}", trigger_actions);
+        if self.autopilot {
             loop {
+                println!("Running onboarding autopilot, {}", trigger_actions_str);
                 thread::sleep(Duration::from_millis(10_000));
                 let _state = transitions::NodeState::init()
-                .maybe_advance(self.trigger_actions)
-                .get_state();
+                    .maybe_advance(trigger_actions)
+                    .get_state();
             }
-
-
+        } 
+        else if self.next {
+            println!("Running manual onboarding, {}", trigger_actions_str);
+            let state = transitions::NodeState::init()
+                .maybe_advance(trigger_actions)
+                .get_state();
+            println!("Onboarding stage at exit: {:?}", state);
         } 
     }
 }
