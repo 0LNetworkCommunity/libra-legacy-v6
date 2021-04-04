d183abde70591a5b84c648d871f209e5d2088167
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2021-04-04 23:45:14 +0000

    svelte reading from chain api

--- ol-cli/web-monitor/src/components/monitor/chain/Info.svelte
+++ ol-cli/web-monitor/src/components/monitor/chain/Info.svelte
@@ -1,8 +1,20 @@
 <script lang="ts">
+  import { onMount } from "svelte";
 
   let epoch: number = 0;
   let round: number = 0;
   let waypoint: string = undefined;
+
+  onMount(async () => {
+    var uri = "http://" + location.host + "/chain";
+    var sse = new EventSource(uri);
+    sse.onmessage = function (msg) {
+      let chain = JSON.parse(msg.data);
+      epoch = chain.epoch;
+      round = chain.height;
+    }
+    console.log(epoch)
+  });
 </script>
 
 <div class="uk-card uk-card-default uk-card-body">
