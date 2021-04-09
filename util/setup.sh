74878dae7bdf469cc6138d2b3b51fe1325d5c959
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2021-04-09 17:51:30 +0000

    setup sh

--- util/setup.sh
+++ util/setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # targeting ububtu
-export NODE_ENV=prod
 apt update
 apt install -y git vim zip jq build-essential cmake clang llvm libgmp-dev secure-delete pkg-config libssl-dev lld
 curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable -y
 export "$HOME/.cargo/bin:$PATH"
 cargo install toml-cli
+cargo install sccache
+
+echo "Manually add `export RUSTC_WRAPPER=sccache` to your bash profile ~/.bashrc`
