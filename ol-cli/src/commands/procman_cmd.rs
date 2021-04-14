fb75a78c1e93cd705cad87feda5cb13911a4e86d
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2021-04-14 00:06:10 +0000

    scaffold for procman

--- ol-cli/src/commands/procman_cmd.rs
+++ ol-cli/src/commands/procman_cmd.rs
@@ -4,6 +4,8 @@
 
 use super::OlCliCmd;
 use abscissa_core::{Command, Options, Runnable};
+use crate::node_health::NodeHealth;
+use crate::management;
 /// `version` subcommand
 #[derive(Command, Debug, Default, Options)]
 pub struct ProcmanCmd {}
@@ -12,7 +14,36 @@ impl Runnable for ProcmanCmd {
     /// Print version message
     fn run(&self) {
         println!("{} {}", OlCliCmd::name(), OlCliCmd::version());
+        let mut n = NodeHealth::new();
+        // Is webserver on?
+        // call http? localhost:3030
+
+        // is DB bootstrapped
+
+
+        // Is in validator in set?
         
-        
+        if n.is_in_validator_set() {}
+        // is node started?
+        if n.node_running() {}
+
+        // restart in validator mode
+
+        // restart in fullnode mode
+
+        // did the node finish sync
+        if NodeHealth::node_is_synced().0 {}
+        // is the node making progress
+        // TODO?
+
+        ////////////// MINING //////////////
+        // does the account exist on chain?
+        n.accounts_exist_on_chain();
+
+        // start miner
+        management::start_miner()
+
+        // are transactions being successfully submitted?
+        // TODO?
     }
 }
