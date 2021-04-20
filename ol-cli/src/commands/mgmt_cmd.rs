357b3ee1e8b68fe2b794e232f0d43b1a00b1e76e
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2021-04-20 00:01:02 +0000

    text

--- ol-cli/src/commands/mgmt_cmd.rs
+++ ol-cli/src/commands/mgmt_cmd.rs
@@ -2,14 +2,7 @@
 
 use crate::{application::app_config, entrypoint, mgmt::management::NodeMode, node::{client, node::Node}};
 use abscissa_core::{Command, Options, Runnable};
-
-/// `mgmt` subcommand
-///
-/// The `Options` proc macro generates an option parser based on the struct
-/// definition, and is defined in the `gumdrop` crate. See their documentation
-/// for a more comprehensive example:
-///
-/// <https://docs.rs/gumdrop/>
+/// management subcommands
 #[derive(Command, Debug, Options)]
 pub struct MgmtCmd {
     #[options(no_short, help = "start node")]
