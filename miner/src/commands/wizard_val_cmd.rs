c79155f25863c0199e6da23d90519168d54b2ad7
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2021-04-24 19:20:07 +0000

    short u for template

--- miner/src/commands/wizard_val_cmd.rs
+++ miner/src/commands/wizard_val_cmd.rs
@@ -31,7 +31,7 @@ pub struct ValWizardCmd {
   skip_fetch_genesis: bool,
   #[options(help = "skip mining a block zero")]
   skip_mining: bool,
-  #[options(short = "t", help = "template account.json to configure from")]
+  #[options(short = "u", help = "template account.json to configure from")]
   template_url: Option<Url>,
   #[options(help = "template account.json to configure from")]
   autopay_file: Option<PathBuf>,
