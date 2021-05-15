01e940e1436b7021b2724717041853055831d3a3
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2021-05-15 16:51:27 +0000

    test-upgrade

--- ol/util/test-upgrade.mk
+++ ol/util/test-upgrade.mk
@@ -37,12 +37,12 @@ stop:
 	killall libra-swarm libra-node | true
 
 get-prev:
-	cd ${SOURCE_PATH} && git reset --hard && git fetch
-	cd ${SOURCE_PATH} && git checkout ${PREV_VERSION}
+	cd ${SOURCE_PATH} && git reset --hard origin && git fetch
+	cd ${SOURCE_PATH} && git checkout ${PREV_VERSION} -f
 
 get-test:
-	cd ${SOURCE_PATH} && git reset --hard && git fetch
-	cd ${SOURCE_PATH} && git checkout ${BRANCH_NAME}
+	cd ${SOURCE_PATH} && git reset --hard origin && git fetch
+	cd ${SOURCE_PATH} && git checkout ${BRANCH_NAME} -f
 
 stdlib:
 	cd ${SOURCE_PATH} && cargo run --release -p stdlib
