92f1b238346bebc7e6cb062fe5987a3b8d770845
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2021-05-16 03:49:28 +0000

    tail

--- ol/util/test-upgrade.mk
+++ ol/util/test-upgrade.mk
@@ -31,7 +31,7 @@ endif
 
 # USAGE: BRANCH_NAME=<latest branch> make -f test-upgrade.mk upgrade-test
 # NOTE: BRANCH_NAME shares semantics with https://github.com/marketplace/actions/get-branch-name
-test: get-prev stdlib start upgrade check tail
+test: get-prev stdlib start upgrade check
 
 start:
 	@echo Building Swarm
