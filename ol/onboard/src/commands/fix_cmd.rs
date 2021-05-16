6d8859cec7a7e919b7e27385188fb636f1c00215
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2021-05-16 16:35:00 +0000

    migrate account.json files

--- ol/onboard/src/commands/fix_cmd.rs
+++ ol/onboard/src/commands/fix_cmd.rs
@@ -2,6 +2,8 @@
 
 #![allow(clippy::never_loop)]
 
+use std::fs;
+
 use crate::{commands::wizard_val_cmd::write_account_json, prelude::app_config};
 use super::wizard_val_cmd::get_autopay_batch;
 use abscissa_core::{Command, Options, Runnable, status_info, time::{DateTime, Utc}};
@@ -25,6 +27,7 @@ impl Runnable for FixCmd {
 pub fn migrate_account_json(cfg: &AppCfg) {
   let (_, _, wallet) = wallet::get_account_from_prompt();
   let home_path = cfg.workspace.node_home.clone();
+  dbg!(&home_path);
   println!("Reading autopay configs");
   let (autopay_batch, autopay_signed) = get_autopay_batch(
         &None,
@@ -38,7 +41,9 @@ pub fn migrate_account_json(cfg: &AppCfg) {
     if account_json_path.exists() {
       let now: DateTime<Utc> = Utc::now();
       let filename = now.format("%Y-%m-%d").to_string() + ".account.json";
-      println!("backing up {:?} to {:?}", account_json_path, cfg.workspace.node_home.clone().join(filename));
+      let backup_path = cfg.workspace.node_home.clone().join(filename);
+      println!("backing up {:?} to {:?}", &account_json_path, &backup_path);
+      fs::copy(&account_json_path, &backup_path).expect("could not backup account.json");
     }
 
     println!("writing account.json to {:?}", cfg.workspace.node_home.clone());
