2424052ea9b48ba9fa93fd95a136fe19063cd998
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2021-05-22 15:01:44 +0000

    cleanup sync functions

--- ol/cli/src/node/client.rs
+++ ol/cli/src/node/client.rs
@@ -1,44 +1,45 @@
 //! `bal` subcommand
 
-use std::path::PathBuf;
-use cli::{libra_client::LibraClient};
-use reqwest::Url;
-use libra_types::{waypoint::Waypoint};
-use anyhow::Error;
-use anyhow::{Result};
 use crate::{config::AppCfg, entrypoint, node::node::Node, prelude::app_config};
+use anyhow::Error;
+use anyhow::Result;
+use cli::libra_client::LibraClient;
+use libra_types::waypoint::Waypoint;
+use reqwest::Url;
+use std::path::PathBuf;
 
 /// returns a LibraClient instance.
 // TODO: Use app config file for params
-pub fn make_client(url: Option<Url>, waypoint: Waypoint) -> Result<LibraClient, Error> { 
+pub fn make_client(url: Option<Url>, waypoint: Waypoint) -> Result<LibraClient, Error> {
     match url {
-        Some(u)=> {
-            LibraClient::new(u, waypoint)
-        },
-        None=> {
-            LibraClient::new(Url::parse("http://localhost:8080")
-                                 .expect("Couldn't create libra client"), waypoint)
-        },
+        Some(u) => LibraClient::new(u, waypoint),
+        None => LibraClient::new(
+            Url::parse("http://localhost:8080").expect("Couldn't create libra client"),
+            waypoint,
+        ),
     }
 }
 
 /// Experimental
 pub fn get_client() -> Option<LibraClient> {
     let entry_args = entrypoint::get_args();
-    // if url and waypoint provided as fn params 
+    // if url and waypoint provided as fn params
     //     return LibraClient::new(url, waypoint);
 
-    // if local node in-sync 
+    // if local node in-sync
     //    return default_local_client()
     // else find and return connect-able upstream node
     let config = app_config();
-    let waypoint = config.get_waypoint(entry_args.swarm_path).expect("could not get waypoint");
+    let waypoint = config
+        .get_waypoint(entry_args.swarm_path)
+        .expect("could not get waypoint");
     for url in config.profile.upstream_nodes.as_ref().unwrap() {
         let mut client = LibraClient::new(url.clone(), waypoint).unwrap();
         // TODO: What's the better way to check we can connect to client?
         let metadata = client.get_metadata();
         dbg!(&metadata);
-        if metadata.is_ok() {   // found a connect-able upstream node
+        if metadata.is_ok() {
+            // found a connect-able upstream node
             return Some(client);
         }
     }
@@ -47,45 +48,71 @@ pub fn get_client() -> Option<LibraClient> {
 }
 
 /// get client type with defaults from toml for remote node
-pub fn default_remote_client(config: &AppCfg, waypoint: Waypoint)  -> Option<(LibraClient, Waypoint)>{
-    let remote_url = config.profile.upstream_nodes.clone().unwrap().into_iter().next().unwrap(); // upstream_node_url.clone();
+pub fn default_remote_client(
+    config: &AppCfg,
+    waypoint: Waypoint,
+) -> Option<(LibraClient, Waypoint)> {
+    let remote_url = config
+        .profile
+        .upstream_nodes
+        .clone()
+        .unwrap()
+        .into_iter()
+        .next()
+        .unwrap(); // upstream_node_url.clone();
     match make_client(Some(remote_url.clone()), waypoint) {
-      Ok(client) => { Some((client, waypoint)) }
-      Err(_) => {None}
+        Ok(client) => Some((client, waypoint)),
+        Err(_) => None,
     }
 }
 
 /// get client type with defaults from toml for local node
-pub fn default_local_client(config: &AppCfg,  waypoint: Waypoint)  -> Option<(LibraClient, Waypoint)>{
-    let local_url = config.profile.default_node.clone().expect("could not get url from configs");
+pub fn default_local_client(
+    config: &AppCfg,
+    waypoint: Waypoint,
+) -> Option<(LibraClient, Waypoint)> {
+    let local_url = config
+        .profile
+        .default_node
+        .clone()
+        .expect("could not get url from configs");
     match make_client(Some(local_url.clone()), waypoint) {
-        Ok(client) => { Some((client, waypoint)) }
-        Err(_) => {None}
+        Ok(client) => Some((client, waypoint)),
+        Err(_) => None,
     }
 }
 
 /// connect a swarm client
-pub fn swarm_test_client(config: &mut AppCfg, swarm_path: PathBuf) -> Option<(LibraClient, Waypoint)> {
+pub fn swarm_test_client(
+    config: &mut AppCfg,
+    swarm_path: PathBuf,
+) -> Option<(LibraClient, Waypoint)> {
     let (url, waypoint) = ol_types::config::get_swarm_configs(swarm_path.clone());
     config.profile.default_node = Some(url.clone());
     config.profile.upstream_nodes = Some(vec![url.clone()]);
 
     match make_client(Some(url.clone()), waypoint) {
-        Ok(client) => {Some((client, waypoint))}
-        Err(_e) => {None}
+        Ok(client) => Some((client, waypoint)),
+        Err(_e) => None,
     }
 }
 
 /// picks what URL to connect to based on sync state. Or returns the client for swarm.
-pub fn pick_client(swarm_path: Option<PathBuf>, config: &mut AppCfg) -> Option<(LibraClient, Waypoint)> {
+pub fn pick_client(
+    swarm_path: Option<PathBuf>,
+    config: &mut AppCfg,
+) -> Option<(LibraClient, Waypoint)> {
     if let Some(path) = swarm_path {
-      return swarm_test_client(config, path);
+        return swarm_test_client(config, path);
     };
-    let waypoint = config.get_waypoint(swarm_path).expect("could not get waypoint");
+    let waypoint = config
+        .get_waypoint(swarm_path)
+        .expect("could not get waypoint");
     // check if is in sync
-    let is_synced: bool = Node::cold_start_is_synced(config, waypoint).0;
-    match is_synced { 
-      true => default_local_client(config, waypoint.clone()),
-      _ => default_remote_client(config, waypoint.clone())
+    if let Ok(s) = Node::check_sync(config, waypoint) {
+        if s.0 {
+            return default_local_client(config, waypoint.clone());
+        }
     }
+    default_remote_client(config, waypoint.clone())
 }
--- ol/cli/src/node/sync.rs
+++ ol/cli/src/node/sync.rs
@@ -4,33 +4,18 @@ use super::node::Node;
 use crate::{config::AppCfg, node::client::*};
 use anyhow::{Error, anyhow, bail};
 use backup_cli::utils::backup_service_client::{BackupServiceClient, BackupServiceClientOpt};
-use cli::libra_client::LibraClient;
 use libra_types::waypoint::Waypoint;
 use libradb::backup::backup_handler::DbState;
 use tokio::runtime::Runtime;
 
 impl Node {
   /// check if node is synced
-  pub fn cold_start_is_synced(config: &AppCfg, waypoint: Waypoint) -> (bool, Option<i64>) {
-    if !Node::node_running() {
-      return (false, None);
-    };
-    match Node::compare_from_config(config, waypoint) {
-      Some(delay) => (within_thresh(delay), Some(delay)),
-      _ => (false, None),
-    }
-  }
-
-  /// check if node is synced
-  pub fn is_synced(&mut self) -> Result<(bool, i64), Error> {
+  pub fn check_sync(config: &AppCfg, waypoint: Waypoint) -> Result<(bool, i64), Error> {
     if !Node::node_running() {
       bail!("Node is not running. Cannot connect to localhost:8080.");
     }
     
-    let wp = self.waypoint().expect("Can not update Waypoint");
-    let mut remote_client = default_remote_client(&self.conf, wp).unwrap().0;
-    dbg!(&remote_client.get_metadata().unwrap().version);
-    dbg!(&self.client.get_metadata().unwrap().version);
+    let mut remote_client = default_remote_client(config, waypoint).unwrap().0;
 
     if let Some(local_db) = get_db_state() {
       let remote_version = remote_client.get_metadata().unwrap().version;
@@ -44,33 +29,36 @@ impl Node {
     Err(anyhow!("Cannot get local db state"))
   }
 
-  /// Compare the nodes from toml config.
-  pub fn compare_from_config(config: &AppCfg, waypoint: Waypoint) -> Option<i64> {
-    let local_client = default_local_client(config, waypoint);
-    let remote_client = default_remote_client(config, waypoint);
-
-    if local_client.is_some() && remote_client.is_some() {
-      return match compare_client_version(
-        &mut local_client.unwrap().0,
-        &mut remote_client.unwrap().0,
-      ) {
-          Ok(delay) => Some(delay),
-          Err(_) => None
-      }
-    }
-    None
+  /// check if node is synced
+  pub fn is_synced(&mut self) -> Result<(bool, i64), Error> {
+    let wp = self.waypoint().expect("Can not update Waypoint");
+    Node::check_sync(&self.conf, wp)
   }
+
+//   /// Compare the nodes from toml config.
+//   // TODO: Deprecated. Not used for sync.
+//   pub fn compare_from_config(config: &AppCfg, waypoint: Waypoint) -> Option<i64> {
+//     let local_client = default_local_client(config, waypoint);
+//     let remote_client = default_remote_client(config, waypoint);
+
+//     if local_client.is_some() && remote_client.is_some() {
+//       return match compare_client_version(
+//         &mut local_client.unwrap().0,
+//         &mut remote_client.unwrap().0,
+//       ) {
+//           Ok(delay) => Some(delay),
+//           Err(_) => None
+//       }
+//     }
+//     None
+//   }
 }
 
-fn compare_client_version(local: &mut LibraClient, remote: &mut LibraClient) -> Result<i64, Error> {
-  let local_meta = local.get_metadata()?;
-  let remote_meta = remote.get_metadata()?;
-  Ok(remote_meta.version as i64 - local_meta.version as i64)
-}
-
-fn within_thresh(delay: i64) -> bool {
-  delay < 10_000
-}
+// fn compare_client_version(local: &mut LibraClient, remote: &mut LibraClient) -> Result<i64, Error> {
+//   let local_meta = local.get_metadata()?;
+//   let remote_meta = remote.get_metadata()?;
+//   Ok(remote_meta.version as i64 - local_meta.version as i64)
+// }
 
 /// get local sync block height
 pub fn get_db_state() -> Option<DbState>{
