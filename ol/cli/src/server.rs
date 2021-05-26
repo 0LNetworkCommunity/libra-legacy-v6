891b313a17fa36d48c571be65589d32e77738813
Adalberto Soares <junynes@gmail.com>
2021-05-26 10:58:29 -0300

    Fixes #500 using source_path for web files on dev env

--- ol/cli/src/server.rs
+++ ol/cli/src/server.rs
@@ -1,7 +1,7 @@
 //! `server`  web monitor http server
 use futures::StreamExt;
 use serde_json::json;
-use std::{convert::Infallible, fs, thread, time::Duration, env::current_dir};
+use std::{convert::Infallible, fs, thread, time::Duration, path::PathBuf};
 use tokio::time::interval;
 use warp::{sse::ServerSentEvent, Filter};
 use ol_types::config::IS_PROD;
@@ -15,16 +15,15 @@ fn sse_vitals(data: Vitals) -> Result<impl ServerSentEvent, Infallible> {
 #[tokio::main]
 /// starts the web server
 pub async fn start_server(node: Node) {
-    let cfg = &node.conf;
+    let workspace = &node.conf.workspace.clone();
 
-    let node_home = cfg.clone().workspace.node_home.clone();
+    let node_home = workspace.node_home.clone();
     //GET check/ (json api for check data)
     let vitals_route = warp::path("vitals").and(warp::get()).map(move || {
         let path = node_home.clone();
         // create server event source from Check object
         let event_stream = interval(Duration::from_secs(10)).map(move |_| {
             let vitals = Vitals::read_json(&path);
-            // let items = health.refresh_checks();
             sse_vitals(vitals)
         });
         // reply using server-sent events
@@ -36,10 +35,11 @@ pub async fn start_server(node: Node) {
         // let obj: Value = serde_json::from_str(&string);
     }));
 
-    let node_home = cfg.clone().workspace.node_home.clone();
+    let node_home = workspace.node_home.clone();
     let epoch_route = warp::path("epoch.json").and(warp::get().map(move || {
         // let node_home = node_home_two.clone();
-        let vitals = Vitals::read_json(&node_home).chain_view.unwrap();
+        let path = node_home.clone();
+        let vitals = Vitals::read_json(&path).chain_view.unwrap();
         let json = json!({
           "epoch": vitals.epoch,
           "waypoint": vitals.waypoint.unwrap().to_string()
@@ -47,14 +47,10 @@ pub async fn start_server(node: Node) {
         json.to_string()
     }));
 
-
-    let node_home = cfg.clone().workspace.node_home.clone();
-    let relative_path = "web-monitor/public/";
-    let web_files = if !*IS_PROD {
-        // for using `npm run dev`
-        current_dir().unwrap().join(&relative_path)
-    } else {
-        node_home.join(&relative_path)
+    let public_dir = PathBuf::from("web-monitor").join("public");
+    let web_files = match *IS_PROD { // TODO: remove IS_PROD
+        true => workspace.node_home.join(&public_dir),
+        false => workspace.source_path.as_ref().unwrap().join(&public_dir), // for using `npm run dev`
     };
 
     //GET /
