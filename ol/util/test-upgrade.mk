ebae575a9b73c1dad7457042216f071876769d2b
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2021-06-02 14:20:21 -0400

    patch test-upgrade

--- ol/util/test-upgrade.mk
+++ ol/util/test-upgrade.mk
@@ -23,11 +23,11 @@ MNEM="talent sunset lizard pill fame nuclear spy noodle basket okay critic grow 
 NUM_NODES = 2
 
 ifndef PREV_VERSION
-PREV_VERSION=v4.3.0
+PREV_VERSION=v4.3.1
 endif
 
 ifndef BRANCH_NAME
-BRANCH_NAME=release-v4.3.1
+BRANCH_NAME=main
 endif
 
 # USAGE: BRANCH_NAME=<latest branch> make -f test-upgrade.mk upgrade-test
