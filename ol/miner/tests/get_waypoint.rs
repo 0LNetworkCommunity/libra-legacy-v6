3dfeb8c19dddadd537b0e226af01dcd1007b6340
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2021-06-05 22:13:02 +0000

    patch tests

--- ol/miner/tests/get_waypoint.rs
+++ ol/miner/tests/get_waypoint.rs
@@ -26,7 +26,6 @@ fn test_waypoint() {
             .expect("Could not write json");
     
     let data = s.get_waypoint(None);
-    dbg!(data);
     let correct: Waypoint = "0:08148a7b1ac857caee13337c77e691734899b7cc82f4968b35455fb91c060df5".parse().unwrap();
     assert_eq!(data.unwrap(), correct, "json value not equal");
     fs::remove_file(path).unwrap();
