1bb9dfed75430f61816c99dab8ee7cd1b965a917
Adalberto Soares <junynes@gmail.com>
2021-06-24 20:47:24 -0300

    web monitor: Moves autopay amount format to the server side. Removes previous amount from autopay table. Display description for each payment type.

--- ol/cli/web-monitor/src/components/autopay/AutoPay.svelte
+++ ol/cli/web-monitor/src/components/autopay/AutoPay.svelte
@@ -1,15 +1,6 @@
 <script lang="ts">
     import "/layout/Style.svelte";
-    
     export let account;
-
-    // TODO: move logic to the server side
-    let formatAmount = (value, type) => {
-      if (type === 0 || type === 1) {
-        return (value / 100).toFixed(2) + "%";
-      }
-      return value.toLocaleString('en-ES');
-    }
   </script>
 
 <div class="uk-card uk-card-default uk-card-body uk-margin-bottom">
@@ -26,20 +17,18 @@
             <th class="uk-text-center">type</th>
             <th class="uk-text-center">payee</th>
             <th class="uk-text-center uk-visible@s">end epoch</th>
-            <th class="uk-text-center uk-visible@s">previous balance</th>
             <th class="uk-text-center">amount</th>
           </tr>
         </thead>
         <tbody>
-          {#each account.auto_pay.payments as {uid, in_type, payee, end_epoch, prev_bal, amt}}
+          {#each account.auto_pay.payments as {uid, type_desc, payee, end_epoch, amount}}
             <tr>
               <td class="uk-text-center">{uid}</td>
-              <td class="uk-text-center">{in_type}</td>
+              <td class="uk-text-center">{type_desc}</td>
               <td class="uk-visible@s uk-text-center">{payee}</td>
               <td class="uk-hidden@s uk-text-truncate">{payee}</td>
               <td class="uk-text-right uk-visible@s">{end_epoch}</td>
-              <td class="uk-text-right uk-visible@s">{prev_bal.toLocaleString('en-ES')}</td>
-              <td class="uk-text-right">{formatAmount(amt, in_type)}</td>
+              <td class="uk-text-right">{amount}</td>
             </tr>
           {/each}
         </tbody>
