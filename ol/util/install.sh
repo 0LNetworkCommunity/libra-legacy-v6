ee6cd4574c5709a4c62a00b5f93347b60a1d8b85
Gökhan Şimşek <g.simsek@paralect.com>
2021-07-05 01:03:25 +0300

    Adding and patching ol/util/*, making it buildable

---
+++ ol/util/install.sh
@@ -0,0 +1,14 @@
+DOWNLOAD_URLS=$(curl -sL https://api.github.com/repos/OLSF/libra/releases/latest | jq -r '.assets[].browser_download_url')
+
+
+for b in $DOWNLOAD_URLS ; do \
+  echo $b ; \
+  echo $b | rev | cut -d"/" -f1 | rev ; \
+  curl  --progress-bar --create-dirs -o /usr/local/bin/$(echo $b | rev | cut -d"/" -f1 | rev) -L $b ; \
+  echo 'downloaded to /usr/local/bin/' ; \
+  chmod 755 /usr/local/bin/$(echo $b | rev | cut -d"/" -f1 | rev) ;\
+done
+
+mkdir ~/.0L/
+mv /usr/local/bin/web-monitor.zip ~/.0L/web-monitor.zip
+unzip ~/.0L/web-monitor.zip
---
+++ ol/util/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# targeting ububtu
+apt update
+apt install -y git vim zip unzip jq build-essential cmake clang llvm libgmp-dev secure-delete pkg-config libssl-dev lld
+curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable -y
+PATH="$HOME/.cargo/bin:$PATH" cargo install toml-cli
+PATH="$HOME/.cargo/bin:$PATH" cargo install sccache
+
+echo "Rust is installed now. Great!\
+\
+To get started you need Cargo's bin directory ($HOME/.cargo/bin) in your PATH\
+environment variable. Next time you log in this will be done\
+automatically.\n\
+To configure your current shell, run: \n\
+source $HOME/.cargo/env\
+"
+
+echo "Manually add 'export RUSTC_WRAPPER=sccache' to your bash profile ~/.bashrc"
