d9cf37073da513f77db0cfab5f43aaa7fbb3a62a
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2021-07-31 19:20:44 +0000

    loop to fetch archive

--- ol/genesis-tools/src/fetch_archive.rs
+++ ol/genesis-tools/src/fetch_archive.rs
@@ -1,17 +1,22 @@
 //! fetches archive files from github
 
-use ol::mgmt::restore::Backup;
+use ol::mgmt::restore::{Backup, fast_forward_db};
 use anyhow::Result;
 
 /// daemon for waiting for an epoch archive to be published
-pub fn wait_for_archive() -> Result<()>{
+pub fn wait_for_archive(epoch: Option<u64>) -> Result<()>{
+  loop {
+    // check every 60 secs.
+    thread::sleep(time::Duration::from_millis(60_000));
+    match fast_forward_db(true, epoch) {
+      Ok(_) => Ok(()),
+      Err(_) => print!("."),
+    };
+  }
+
   Ok(())
 }
 
-/// fetch archive from epoch archives
-fn fetch_archive() -> Result<()> {
-  Ok(())
-}
 
 #[test]
 fn test_fetch() {
