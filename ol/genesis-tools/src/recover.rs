533e245b7902db1b691a5771bf5cb18d1888cd76
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2021-08-05 14:29:25 +0000

    GenesisRecovery recovery struct

--- ol/genesis-tools/src/recover.rs
+++ ol/genesis-tools/src/recover.rs
@@ -3,9 +3,31 @@
 use anyhow::{Error, bail};
 use libra_types::{account_state::AccountState, validator_config::ValidatorConfigResource, write_set::WriteSetMut};
 use move_core_types::move_resource::MoveResource;
+use ol_types::miner_state::MinerStateResource;
 
+enum AccountRole {
+  System,
+  Validator,
+  Operator,
+  EndUser
+}
 
-fn get_recovery_structs(account_state: &AccountState) -> Result<ValidatorConfigResource, Error> {
+enum WalletType {
+  None,
+  Slow,
+  Community,
+}
+struct GenesisRecovery {
+  val_cfg: ValidatorConfigResource,
+  balance: u64,
+  miner_state: MinerStateResource,
+  role: AccountRole,
+  wallet_type: WalletType,
+  // TODO: Fullnode State? // rust struct does not exist
+  // TODO: Autopay? // rust struct does not exist
+}
+/// create a recovery struct from an account state.
+pub fn get_recovery_struct(account_state: &AccountState) -> Result<GenesisRecovery, Error> {
         let _ws = WriteSetMut::new(vec![]);
         if let Some(address) = account_state.get_account_address()? {
             // iterate over all the account's resources\
