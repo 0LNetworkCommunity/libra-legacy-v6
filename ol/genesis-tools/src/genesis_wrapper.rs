e9366c8deaae4b6c92f86317e53ea53c148f123f
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2021-08-07 14:29:59 +0000

    clear

--- ol/genesis-tools/src/genesis_wrapper.rs
+++ ol/genesis-tools/src/genesis_wrapper.rs
@@ -1,48 +1,42 @@
 //! genesis-wrapper
 
 use crate::recover::{GenesisRecovery, RecoveryFile};
-
-use libra_types::{account_address::AccountAddress, transaction::{authenticator::AuthenticationKey}};
-
+use libra_types::account_address::AccountAddress;
+use vm_genesis::ValRecover;
+use crate::recover::AccountRole::*;
 use std::path::PathBuf;
-use vm_genesis::*;
 
 /// Make recovery file in format needed
-pub fn make_recovery_file(
-    recover: Vec<GenesisRecovery>,
-    _output_path: PathBuf,
-) {
+pub fn make_recovery_file(recover: Vec<GenesisRecovery>, _output_path: PathBuf) {
     // read file
     let mut file = RecoveryFile::default();
 
     for i in recover {
         let val_account: AccountAddress = i.account;
-        // get deduplicated validators info 
+        // get deduplicated validators info
         match i.role {
-            crate::recover::AccountRole::System => todo!(),
-            crate::recover::AccountRole::Validator => {
-              
-            },
-            crate::recover::AccountRole::Operator => todo!(),
-            crate::recover::AccountRole::EndUser => todo!(),
+            System => todo!(),
+            Validator => {
+                // prevent duplicate fields
+                if file
+                    .vals
+                    .iter()
+                    .find(|&a| a.val_account == val_account)
+                    .is_none()
+                {
+                    file.vals.push(ValRecover {
+                        val_account,
+                        operator_delegated_account: i.val_cfg.unwrap().delegated_account.unwrap(),
+                        val_auth_key: i.auth_key.unwrap(),
+                    });
+                }
+            }
+            Operator => todo!(),
+            EndUser => todo!(),
         }
         // get deduplicated operator info
-        let operator_delegated_account: AccountAddress = i.val_cfg.unwrap().delegated_account.unwrap();
-        let val_auth_key: AuthenticationKey = i.auth_key.unwrap();
-        
-        // prevent duplicate fields
-        if file.vals
-        .iter()
-        .find(|&a|{ a.val_account == val_account})
-        .is_none() {
-            file.vals.push(ValRecover {
-              val_account,
-              operator_delegated_account,
-              val_auth_key,
-          });
-        }
-        // file.vals.sort_by(|a, b| b.val_account.cmp(&a.val_account));
 
+        // file.vals.sort_by(|a, b| b.val_account.cmp(&a.val_account));
     }
 
     // get operators to recover
