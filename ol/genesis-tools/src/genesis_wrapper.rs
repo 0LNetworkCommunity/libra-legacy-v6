695c8ebcf59d9b47530fb24b9221fd582dfe4f7c
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2021-08-07 13:58:43 +0000

    cargo fix

--- ol/genesis-tools/src/genesis_wrapper.rs
+++ ol/genesis-tools/src/genesis_wrapper.rs
@@ -1,16 +1,16 @@
 //! genesis-wrapper
 
 use crate::recover::GenesisRecovery;
-use anyhow::Error;
+
 use libra_types::{account_address::AccountAddress, chain_id::ChainId, on_chain_config::VMPublishingOption, transaction::{ChangeSet, Transaction, WriteSetPayload, authenticator::AuthenticationKey}};
-use libra_vm::{data_cache::StateViewCache, txn_effects_to_writeset_and_events};
-use serde_json::Value;
+
+
 use std::path::PathBuf;
 use vm_genesis::*;
 
 pub fn make_recovery_file(
     recover: Vec<GenesisRecovery>,
-    output_path: PathBuf,
+    _output_path: PathBuf,
 ) {
     // read file
     let mut vals: Vec<ValRecover> = vec![];
