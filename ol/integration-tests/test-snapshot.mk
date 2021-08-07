4b4065bbadb3fb5256cccfef20cecac0fd00a1f6
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2021-08-07 19:07:18 +0000

    makefile

--- ol/integration-tests/test-snapshot.mk
+++ ol/integration-tests/test-snapshot.mk
@@ -1,4 +1,11 @@
-cargo run -p ol-genesis-tools -- --path ./ol/fixtures/state-snapshot/194/state_ver_74694920.0889/
+NODE_ENV="test"
 
-cargo build -p libra-node -p cli && NODE_ENV="test" cargo run -p libra-swarm -- --libra-node target/debug/libra-node -c ~/.0L/swarm_temp -n 1 -s --cli-path target/debug/cli --genesis-blob-path ~/.0L/genesis_from_snapshot.blob
+genesis:
+	cargo run -p ol-genesis-tools -- --genesis ~/.0L/genesis_from_snapshot.blob --snapshot ../fixtures/state-snapshot/194/state_ver_74694920.0889/state.manifest
+
+build: 
+	cargo build -p libra-node -p cli
+
+swarm: build
+	 cargo run -p libra-swarm -- --libra-node target/debug/libra-node -c ~/.0L/swarm_temp -n 1 -s --cli-path target/debug/cli --genesis-blob-path ~/.0L/genesis_from_snapshot.blob
 
