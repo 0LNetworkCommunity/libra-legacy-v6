f8af7217aaca8c37ee1c08e27528f566e586333f
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2021-08-07 19:26:58 +0000

    makefile

--- ol/integration-tests/test-snapshot.mk
+++ ol/integration-tests/test-snapshot.mk
@@ -1,11 +1,17 @@
-NODE_ENV="test"
+SHELL=/usr/bin/env bash
+DATA_PATH = ${HOME}/.0L
+SWARM_TEMP = ${DATA_PATH}/swarm_temp
+SOURCE_PATH = ${HOME}/libra/
+UNAME := $(shell uname)
+
+NODE_ENV=test
 
 genesis:
-	cargo run -p ol-genesis-tools -- --genesis ~/.0L/genesis_from_snapshot.blob --snapshot ../fixtures/state-snapshot/194/state_ver_74694920.0889/state.manifest
+	cargo run -p ol-genesis-tools -- --genesis ${DATA_PATH}/genesis_from_snapshot.blob --snapshot ../fixtures/state-snapshot/194/state_ver_74694920.0889/state.manifest
 
 build: 
 	cargo build -p libra-node -p cli
 
 swarm: build
-	 cargo run -p libra-swarm -- --libra-node target/debug/libra-node -c ~/.0L/swarm_temp -n 1 -s --cli-path target/debug/cli --genesis-blob-path ~/.0L/genesis_from_snapshot.blob
+	 cargo run -p libra-swarm -- --libra-node ${SOURCE_PATH}/target/debug/libra-node -c ~/.0L/swarm_temp -n 1 -s --cli-path ${SOURCE_PATH}/target/debug/cli --genesis-blob-path ${DATA_PATH}/genesis_from_snapshot.blob
 
