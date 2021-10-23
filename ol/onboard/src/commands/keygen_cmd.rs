82df055bbc7aa4a68ce6dcdcaa9a7bbe1d072130
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2021-10-23 19:10:06 +0000

    authentication key

--- ol/onboard/src/commands/keygen_cmd.rs
+++ ol/onboard/src/commands/keygen_cmd.rs
@@ -6,11 +6,23 @@ use abscissa_core::{Command, Options, Runnable};
 use ol_keys::wallet;
 /// `keygen` subcommand
 #[derive(Command, Debug, Default, Options)]
-pub struct KeygenCmd {}
+pub struct KeygenCmd {
+  #[options(short="w", help = "who am I?")]
+  whoami: bool,
+}
 
 
 impl Runnable for KeygenCmd {
     fn run(&self) {
-        wallet::keygen();
+        if self.whoami {
+          let (auth, addr, wallet) = wallet::get_account_from_prompt();
+          println!("\n0L ACCOUNT\n");
+          println!("address: {}", addr);
+          println!("long address (authentication key): {}\n", auth);
+
+        } else {
+          wallet::keygen();
+        }
+        
     }
 }
