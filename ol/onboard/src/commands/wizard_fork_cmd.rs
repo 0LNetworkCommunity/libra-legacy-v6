a35af4bb676143116388074a5fbac31a2f0ba6b0
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2021-10-23 13:00:12 +0000

    text

--- ol/onboard/src/commands/wizard_fork_cmd.rs
+++ ol/onboard/src/commands/wizard_fork_cmd.rs
@@ -65,7 +65,7 @@ impl Runnable for ForkCmd {
         status_info!("\nValidator Config Wizard.", "Next you'll enter your mnemonic and some other info to configure your validator node and on-chain account. If you haven't yet generated keys, run the standalone keygen tool with 'onboard keygen'.");
 
         if !self.skip_mining {
-          println!("\nYour first 0L proof-of-work will be mined now. Expect this to take up to 15 minutes on modern CPUs.\n");
+          println!("\nYour first 0L proof-of-work will be mined now. Expect this take at least 30 minutes on modern CPUs.\n");
         }
 
         let entry_args = entrypoint::get_args();
