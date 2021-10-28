cb8340c9a4bac50e15089100f8aa943dea72bc6f
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2021-10-28 23:50:10 +0000

    try different erro handling

--- json-rpc/types/src/views.rs
+++ json-rpc/types/src/views.rs
@@ -1449,7 +1449,7 @@ impl TryFrom<AccountState> for TowerStateResourceView {
     type Error = Error;
 
     fn try_from(state: AccountState) -> Result<TowerStateResourceView, Error> {
-        let m = state.get_miner_state()?.unwrap_or(bail!("could not get tower state"));
+      if let Some(m) = state.get_miner_state()? {
         Ok(TowerStateResourceView {
             previous_proof_hash: BytesView::from( m.previous_proof_hash),
             verified_tower_height: m.verified_tower_height, // user's latest verified_tower_height
@@ -1459,6 +1459,9 @@ impl TryFrom<AccountState> for TowerStateResourceView {
             contiguous_epochs_validating_and_mining: m.contiguous_epochs_validating_and_mining,
             epochs_since_last_account_creation: m.epochs_since_last_account_creation
         })
+      } else {
+        bail!("could not get tower state")
+      }
     }
 }
 
@@ -1482,10 +1485,14 @@ impl TryFrom<AccountState> for OracleUpgradeStateView {
     type Error = Error;
 
     fn try_from(state: AccountState) -> Result<OracleUpgradeStateView, Self::Error> {
-        let m = state.get_oracle_state()?.unwrap_or(bail!("could not get upgrade oracle state"));
-        // TODO: duplication UpgradeResource is the same as OracleUpgradeStateView
-        Ok(OracleUpgradeStateView {
-            upgrade: m.upgrade,
-        })
+        if let Some(m) = state.get_oracle_state()? {
+          // TODO: duplication UpgradeResource is the same as OracleUpgradeStateView
+          Ok(OracleUpgradeStateView {
+              upgrade: m.upgrade,
+          })
+        } else {
+          Err(Error::msg("could not get upgrade oracle data"))
+        }
+
     }
 }
