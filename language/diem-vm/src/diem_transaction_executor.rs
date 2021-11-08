342b64aca075b48f12a5d092785f8371cb95d067
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2021-11-08 19:58:07 +0000

    update printing from vm, less noise

--- language/diem-vm/src/diem_transaction_executor.rs
+++ language/diem-vm/src/diem_transaction_executor.rs
@@ -489,9 +489,10 @@ impl DiemVM {
         let mut session = self.0.new_session(storage);
 
         let (round, timestamp, previous_vote, proposer) = block_metadata.clone().into_inner();
-
-        println!("========================================== {} ==========================================", round);
-
+        
+        if (round < 100) || (round % 100 == 0){
+          println!("====================================== {} ======================================", round);
+        }
         
         let args = serialize_values(&vec![
             MoveValue::Signer(txn_data.sender),
