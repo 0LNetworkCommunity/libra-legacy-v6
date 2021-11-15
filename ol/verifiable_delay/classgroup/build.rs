4b2b79c31e22c765efdc5beab617a5069987d96d
Gokhan Simsek <g.simsek@paralect.com>
2021-11-15 11:28:30 +0300

    Use cross-platform path

--- ol/verifiable_delay/classgroup/build.rs
+++ ol/verifiable_delay/classgroup/build.rs
@@ -1,7 +1,9 @@
 #[allow(non_snake_case)]
 
+use std::path::Path;
+
 fn main() {
   let PROJECT_DIR = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-  let GMP_LIB_DIR = PROJECT_DIR + "\\gmp\\";
-  println!("cargo:rustc-link-search={}", GMP_LIB_DIR);
+  let GMP_LIB_DIR = Path::new(&PROJECT_DIR).join("gmp");
+  println!("cargo:rustc-link-search={}", GMP_LIB_DIR.to_str().unwrap());
 }
