219fb8629e8a3cc98f2c646883171229ab89976c
coin1111 <celticsnow123@gmail.com>
2021-12-10 03:05:00 +0000

    basic tests

--- language/move-lang/functional-tests/tests/0L/ethsign/EthSignature.move
+++ language/move-lang/functional-tests/tests/0L/ethsign/EthSignature.move
@@ -8,17 +8,20 @@ script{
     use 0x1::EthSignature;
     fun main() {
 
+
+        // test1 : recover public key
         let data = b"Some data";
         let expected_pubkey = x"2c7536E3605D9C16a7a3D7b1898e529396a65c23";
         let sign = x"b91467e570a6466aa9e9876cbcd013baba02900b8979d43fe208a4a4f339f5fd6007e74cd82e037b800186422fc2da167c747ef045e5d18a5f5d4300f8e1a0291c";
-
-        // test1 : recover public key
-        let pubkey = EthSignature::recover(copy sign, copy data);
+        let pubkey = EthSignature::recover(sign, data);
         assert(expected_pubkey == pubkey, 1);
 
         // test2 : verify signature
-        //let res = EthSignature::verify(copy sign, copy pubkey, copy data);
-        //assert(res == true, 1);
+        data = b"Some data";
+        pubkey = x"2c7536E3605D9C16a7a3D7b1898e529396a65c23";
+        sign = x"b91467e570a6466aa9e9876cbcd013baba02900b8979d43fe208a4a4f339f5fd6007e74cd82e037b800186422fc2da167c747ef045e5d18a5f5d4300f8e1a0291c";
+        let res = EthSignature::verify(sign, pubkey, data);
+        assert(res == true, 1);
 
     }
 }
--- language/move-vm/natives/src/ol_eth_signature.rs
+++ language/move-vm/natives/src/ol_eth_signature.rs
@@ -13,8 +13,6 @@ use smallvec::smallvec;
 use std::{collections::VecDeque, convert::TryFrom};
 use ethers::core::types::{H160, Signature}; 
 
-use std::{thread, time};
-
 pub fn native_eth_signature_recover(
     context: &impl NativeContext,
     _ty_args: Vec<Type>,
@@ -36,14 +34,14 @@ pub fn native_eth_signature_recover(
         msg_bytes.len(),
     );
 
-    let sig = match ethers::core::types::Signature::try_from(&sig_bytes[..]) {
+    let sig = match ethers::core::types::Signature::try_from(sig_bytes.as_slice()) {
         Ok(sig) => sig,
         Err(_) => {
             return Ok(NativeResult::ok(cost, smallvec![Value::bool(false)]));
         }
     };
 
-    let pubkey = match sig.recover(&msg_bytes[..]) {
+    let pubkey = match sig.recover(msg_bytes.as_slice()) {
         Ok(pubkey) => pubkey,
         Err(_) => {
             return Ok(NativeResult::ok(cost, smallvec![Value::bool(false)]));
@@ -62,7 +60,7 @@ pub fn native_eth_signature_verify(
     mut arguments: VecDeque<Value>,
 ) -> PartialVMResult<NativeResult> {
     debug_assert!(_ty_args.is_empty());
-    debug_assert!(arguments.len() == 1);
+    debug_assert!(arguments.len() == 3);
 
     let msg_bytes = pop_arg!(arguments, Vec<u8>);
     let pubkey_bytes = pop_arg!(arguments, Vec<u8>);
@@ -74,24 +72,18 @@ pub fn native_eth_signature_verify(
         msg_bytes.len(),
     );
 
-    let sig = match ethers::core::types::Signature::try_from(&sig_bytes[..]) {
+    let sig = match ethers::core::types::Signature::try_from(sig_bytes.as_slice()) {
         Ok(sig) => sig,
         Err(_) => {
             return Ok(NativeResult::ok(cost, smallvec![Value::bool(false)]));
         }
     };
 
-    let pubkey = ethers::core::types::H160::from_slice(&pubkey_bytes[..]);
+    let pubkey = ethers::core::types::H160::from_slice(pubkey_bytes.as_slice());
 
-    let result = match sig.verify(&msg_bytes[..],pubkey) {
-        Ok(result) => result,
-        Err(_) => {
-            return Ok(NativeResult::ok(cost, smallvec![Value::bool(false)]));
-        }
-    };
-
+    let verify_result = sig.verify(msg_bytes.as_slice(),pubkey).is_ok();
     Ok(NativeResult::ok(
         cost,
-        smallvec![Value::bool(true)],
+        smallvec![Value::bool(verify_result)],
     ))
 }
