28af5564fa0af7ef955a8340af070009fd4ec1cf
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2022-01-19 19:36:29 +0000

    clean

--- language/diem-vm/src/system_module_names.rs
+++ language/diem-vm/src/system_module_names.rs
@@ -38,11 +38,6 @@ pub static CHECK_UPGRADE: Lazy<Identifier> =
     Lazy::new(|| Identifier::new("check_upgrade").unwrap());
 static UPGRADE_MODULE_NAME: Lazy<Identifier> =
     Lazy::new(|| Identifier::new("Upgrade").unwrap());
-
-// // TODO: this will be deprecated in 5.0.11
-// pub static RESET_PAYLOAD: Lazy<Identifier> =
-//     Lazy::new(|| Identifier::new("reset_payload").unwrap());
-
 pub static UPGRADE_RECONFIG: Lazy<Identifier> =
     Lazy::new(|| Identifier::new("upgrade_reconfig").unwrap());
 //////// 0L end ////////
