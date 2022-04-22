65a49ff38dcd4e5186fec3de44eaa1403a04ec2e
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2022-04-22 03:50:02 +0000

    patch

--- language/diem-tools/writeset-transaction-generator/src/main.rs
+++ language/diem-tools/writeset-transaction-generator/src/main.rs
@@ -11,7 +11,7 @@ use diem_types::{
 use diem_writeset_generator::{
     create_release, encode_custom_script, encode_halt_network_payload,
     encode_remove_validators_payload, script_bulk_update_vals_payload, release_flow::artifacts::load_latest_artifact,
-    verify_release, ol_writeset_stdlib_upgrade, ol_create_reconfig_payload, ol_writset_encode_rescue, ol_writset_update_timestamp, ol_writeset_force_boundary, ol_writeset_set_testnet, ol_writeset_debug_epoch, ol_writeset_update_epoch_time, ol_writeset_ancestry, ol_writset_encode_migrations
+    verify_release, ol_writeset_stdlib_upgrade, ol_create_reconfig_payload, ol_writset_encode_rescue, ol_writset_update_timestamp, ol_writeset_force_boundary, ol_writeset_set_testnet, ol_writeset_debug_epoch, ol_writeset_update_epoch_time, ol_writeset_ancestry, ol_writset_encode_migrations, ol_debug
 };
 use move_binary_format::CompiledModule;
 use std::path::PathBuf;
