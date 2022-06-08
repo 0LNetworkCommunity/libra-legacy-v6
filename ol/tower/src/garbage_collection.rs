7693c37b5e810190dd14b552c545e3b4c63dc29a
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2022-06-08 22:51:42 +0000

    scaffold

--- ol/tower/src/garbage_collection.rs
+++ ol/tower/src/garbage_collection.rs
@@ -1,6 +1,22 @@
 //! garbage collection
 
+use std::path::PathBuf;
 
-// check remaining proofs in backlog.
-// if they all fail, move the list to a trash file
 
+/// check remaining proofs in backlog.
+/// if they all fail, move the list to a trash file
+pub fn find_discontinous_proofs() -> Option<Vec<PathBuf>> {
+  // start from last known proof on chain.
+  todo!()
+}
+
+/// if a proof can't be verified this epoch, the subsequent proofs will not be valid.
+pub fn find_proofs_chain_with_deprecated_params() -> Option<Vec<PathBuf>> {
+  // start from last known proof on chain.
+  todo!()
+}
+
+/// take list of proofs and save in garbage file
+pub fn put_in_trash() -> anyhow::Result<()> {
+  todo!()
+}
