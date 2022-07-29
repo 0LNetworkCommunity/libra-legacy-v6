0d86569070e656d9808bdd9b584442ef416facdc
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2022-07-29 15:44:02 -0400

    increase state sync tick interval to prevent timeouts
    Co-authored-by: 0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>

--- config/src/config/state_sync_config.rs
+++ config/src/config/state_sync_config.rs
@@ -54,7 +54,7 @@ impl Default for StateSyncConfig {
             mempool_commit_timeout_ms: 5_000,
             multicast_timeout_ms: 30_000,
             sync_request_timeout_ms: 60_000, ///////// 0L ////////
-            tick_interval_ms: 500, ///////// 0L ////////
+            tick_interval_ms: 3000, ///////// 0L ////////
             data_streaming_service: DataStreamingServiceConfig::default(),
             diem_data_client: DiemDataClientConfig::default(),
             state_sync_driver: StateSyncDriverConfig::default(),
