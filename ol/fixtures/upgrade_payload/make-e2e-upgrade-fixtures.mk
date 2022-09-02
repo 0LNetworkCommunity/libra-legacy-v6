ca7e38c12df9e9340ad6dc3f794839e0c9cfef18
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2022-09-02 14:36:32 -0400

    patch upgrade fixtures

--- ol/fixtures/upgrade_payload/make-e2e-upgrade-fixtures.mk
+++ ol/fixtures/upgrade_payload/make-e2e-upgrade-fixtures.mk
@@ -1,13 +1,13 @@
 SHELL=/usr/bin/env bash
 
 ifndef SOURCE_PATH
-SOURCE_PATH = ${HOME}/libra/
+SOURCE_PATH = ${HOME}/libra
 endif
 
 LIBRA_PATH = ${SOURCE_PATH}
-FIXTURES_PATH = ${LIBRA_PATH}/ol/fixtures/upgrade_payload/
-DF_SRC_PATH = ${LIBRA_PATH}/diem-move/diem-framework/DPN/sources/
-DF_PATH = ${LIBRA_PATH}/diem-move/diem-framework/
+FIXTURES_PATH = ${LIBRA_PATH}/ol/fixtures/upgrade_payload
+DF_SRC_PATH = ${LIBRA_PATH}/diem-move/diem-framework/DPN/sources
+DF_PATH = ${LIBRA_PATH}/diem-move/diem-framework
 
 # Create foo_stdlib.mv which contains"foo" symbol(fn)
 fixtures: rename-files stdlib check-foo copy reverse-rename stdlib-again
@@ -33,7 +33,7 @@ check-foo:
 	grep ${DF_PATH}/staged/stdlib.mv -e foo
 
 copy:
-	cp ${DF_PATH}/staged/stdlib.mv ${FIXTURES_PATH}/foo_stdlib.mv
+	cp ${DF_PATH}/DPN/releases/artifacts/currnt/staged/stdlib.mv ${FIXTURES_PATH}/foo_stdlib.mv
 
 # cp ${DF_PATH}/releases/artifacts/current/script_abis/ol_e2e_test_upgrade_foo_tx/ol_oracle_upgrade_foo_tx.abi \ 
 #    ${FIXTURES_PATH}/tx_scripts/ol_oracle_upgrade_foo_tx.abi
