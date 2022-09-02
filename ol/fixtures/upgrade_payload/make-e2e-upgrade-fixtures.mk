469c2acdf4b11ae659f3ba442a2a84403d6d03bf
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2022-09-02 14:45:05 -0400

    fix upgrade fixtures makefile

--- ol/fixtures/upgrade_payload/make-e2e-upgrade-fixtures.mk
+++ ol/fixtures/upgrade_payload/make-e2e-upgrade-fixtures.mk
@@ -8,16 +8,16 @@ LIBRA_PATH = ${SOURCE_PATH}
 FIXTURES_PATH = ${LIBRA_PATH}/ol/fixtures/upgrade_payload
 DF_SRC_PATH = ${LIBRA_PATH}/diem-move/diem-framework/DPN/sources
 DF_PATH = ${LIBRA_PATH}/diem-move/diem-framework
-
+MOVE_BIN_PATH = ${DF_PATH}/DPN/releases/artifacts/current/staged/stdlib.mv
 # Create foo_stdlib.mv which contains"foo" symbol(fn)
 fixtures: rename-files stdlib check-foo copy reverse-rename stdlib-again
 
 stdlib:
-	cd ${DF_PATH} && cargo r --release -- --create-upgrade-payload
+	cd ${SOURCE_PATH} && make stdlib
 
 stdlib-again:
 # TODO: can't run recipes twice?
-	cd ${DF_PATH} && cargo r --release -- --create-upgrade-payload
+	cd ${SOURCE_PATH} && make stdlib
 
 rename-files:
 # Module rename
@@ -30,10 +30,10 @@ rename-files:
 
 check-foo:
 # checks the foo function exists in the compile
-	grep ${DF_PATH}/staged/stdlib.mv -e foo
+	grep ${MOVE_BIN_PATH} -e foo
 
 copy:
-	cp ${DF_PATH}/DPN/releases/artifacts/currnt/staged/stdlib.mv ${FIXTURES_PATH}/foo_stdlib.mv
+	cp ${MOVE_BIN_PATH} ${FIXTURES_PATH}/foo_stdlib.mv
 
 # cp ${DF_PATH}/releases/artifacts/current/script_abis/ol_e2e_test_upgrade_foo_tx/ol_oracle_upgrade_foo_tx.abi \ 
 #    ${FIXTURES_PATH}/tx_scripts/ol_oracle_upgrade_foo_tx.abi
