25fe2734b9fd1bf72126b6d74e0330c477f02cc9
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2023-02-01 18:57:54 -0500

    query nodes to check if onchain state is the same as recovery.

--- ol/genesis-tools/tests/start_node_from_blob.rs
+++ ol/genesis-tools/tests/start_node_from_blob.rs
@@ -3,6 +3,9 @@ use std::io::BufReader;
 use std::path::PathBuf;
 use std::process::Command;
 use std::process::Stdio;
+use diem_json_rpc::views::AccountView;
+use diem_types::account_address::AccountAddress;
+// use move_core_types::move_resource::MoveResource;
 
 #[test]
 fn start_test_node() {
@@ -28,12 +31,15 @@ fn start_test_node() {
 
     let stdout = cmd.stdout.take().expect("no stdout");
 
+    // Listen to stdout and wait until the tenth block is reported.
     BufReader::new(stdout)
     .lines()
     .find(|e| {
         dbg!(&e);
         e.as_ref().unwrap().contains("==== 10")
     });
+
+
 }
 
 fn blob_path() -> PathBuf {
@@ -46,4 +52,36 @@ fn blob_path() -> PathBuf {
         .unwrap()
         .join("ol/fixtures/rescue/sample_export_recovery.json")
         .to_owned()
+}
+
+
+
+fn post_node_json(a: AccountAddress) -> anyhow::Result<AccountView>{
+  let url = format!("http://0.0.0.0:8080/v1");
+
+  let query = serde_json::json!( {
+    "jsonrpc":"2.0",
+    "method":"get_account",
+    "params":[&a.to_string()],
+    "id":1
+  });
+
+
+  let client = reqwest::blocking::Client::new();
+  let res: serde_json::Value = client.post(url)
+    .json(&query)
+    .send()?
+    .json()?;
+
+  let body = res["result"].to_owned();
+  dbg!(&body);
+  let view = serde_json::from_value(body)?;
+  Ok(view)
+}
+
+#[test]
+fn meta_test_node() {
+  // NOTE: start cargo r -p diem-node -- --test
+  let r = post_node_json(AccountAddress::ZERO);
+  dbg!(&r);
 }
