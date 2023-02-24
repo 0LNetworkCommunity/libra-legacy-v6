7a435f75e61b6a8a160ceadd4ab9d04e03382c26
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2023-02-24 18:17:12 -0500

    patch regression in vouches

--- diem-move/vm-genesis/src/lib.rs
+++ diem-move/vm-genesis/src/lib.rs
@@ -631,23 +631,23 @@ fn create_and_initialize_owners_operators(
     println!("\nVALIDATOR SET");
     print_list.into_iter().for_each(|v| println!("{}", v));
 
-    // for v in validators {
-    //     let all_vals: Vec<AccountAddress> = validators.iter()
-    //         .map(|v|{ v.address }).collect();
-    //     let mut vals = all_vals.clone();
-    //     vals.retain(|el|{ el != &v.address});
-    //     exec_function(
-    //         session,
-    //         "Vouch",
-    //         "vm_migrate",
-    //         vec![],
-    //         serialize_values(&vec![
-    //             MoveValue::Signer(diem_root_address),
-    //             MoveValue::Address(v.address),
-    //             MoveValue::vector_address(vals),
-    //         ]),
-    //     );
-    // }
+    for v in validators {
+        let all_vals: Vec<AccountAddress> = validators.iter()
+            .map(|v|{ v.address }).collect();
+        let mut vals = all_vals.clone();
+        vals.retain(|el|{ el != &v.address});
+        exec_function(
+            session,
+            "Vouch",
+            "vm_migrate",
+            vec![],
+            serialize_values(&vec![
+                MoveValue::Signer(diem_root_address),
+                MoveValue::Address(v.address),
+                MoveValue::vector_address(vals),
+            ]),
+        );
+    }
 }
 
 // //////// 0L ///////
