a7800b1926e54c3d78fd4e9f3983655879fa2a39
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2023-03-02 14:35:51 -0500

    api endpoint for a struct on an account.

--- api/src/index.rs
+++ api/src/index.rs
@@ -52,6 +52,7 @@ pub fn routes(context: Context) -> impl Filter<Extract = impl Reply, Error = Inf
         .or(accounts::get_account_modules_by_ledger_version(
             context.clone(),
         ))
+        .or(accounts::get_account_struct(context.clone())) //////// 0L ////////
         .or(transactions::get_transaction(context.clone()))
         .or(transactions::get_transactions(context.clone()))
         .or(transactions::get_account_transactions(context.clone()))
